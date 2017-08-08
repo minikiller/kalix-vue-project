248f44fc3ed48209d0548715d572d35f0f21bc8a
sunlingfeng <minikiller@qq.com>
2017-08-08 10:27:13 +0800

    change style

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from "vue";
-import axios from "axios";
-import VueAxios from "vue-axios";
-import Vuex from "vuex";
-import App from "./App";
-import router from "./router";
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import Vuex from 'vuex'
+import App from './App'
+import router from './router'
 
 Vue.config.productionTip = false
 Vue.use(Vuex)
@@ -15,5 +15,5 @@ new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App }
+  components: {App}
 })
