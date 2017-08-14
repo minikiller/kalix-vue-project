dac6c41ade37ef987aa2c23ae20ae75f01521cb9
sunlingfeng <minikiller@qq.com>
2017-08-14 11:09:52 +0800

    global set component

---
+++ src/views/demo/world.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1> first thing to need {{msg}}</h1>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    data() {
+      return {
+        msg: 'hello world'
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus">
+
+</style>
