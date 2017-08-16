a228cb15cd7d012fe9fbdffe158dc4f8fe2cc80f
juezhan <11991762@qq.com>
2017-08-16 16:06:08 +0800

    update user.vue add userSearch.vue,userSearchBak.vue baseSearch.vue util.js

---
+++ src/views/admin/user/baseSearch.vue
@@ -0,0 +1,126 @@
+<template lang="pug">
+  div.kalix-search
+    div.kalix-search-hd
+      i.iconfont.icon-query
+      | {{title}}
+    div.kalix-search-bd
+      el-form.search-container(ref="searchForm" v-bind:rules="formRules" v-bind:model="form" v-bind:inline="true")
+        slot(name="searchFormSlot")
+          el-form-item(v-for="item in filters" v-bind:label="item.label" v-bind:prop="item.prop" v-bind:key="item.prop")
+            el-select(v-if="item.type==='select'" v-model="form[item.prop]")
+              el-option(v-for="option in item.options" v-bind:key="option.value" v-bind:label="option.label" v-bind:value="option.value")
+            el-input-number(v-else-if="item.type==='number'" v-model="form[item.prop]")
+            el-input(v-else v-model="form[item.prop]")
+        el-form-item
+          el-button(type="primary" v-on:click="onSubmitClick")
+            i.iconfont.icon-query
+            | 查询
+          el-button(type="success" v-on:click="onResetClick")
+            i.iconfont.icon-reset
+            | 重置
+</template>
+
+<script>
+  import {strToUnicode} from 'common/unicode-convert'
+  import {isEmptyObject} from 'common/util'
+
+  const ON_SEARCH = 'onSearch'
+  export default {
+    data() {
+      return {
+        form: {},
+        isSearch: false
+      }
+    },
+    props: {
+      searchForm: {
+        type: Object
+      },
+      title: {
+        type: String,
+        default: ''
+      },
+      formRules: {},
+      filters: {
+        type: Array
+      }
+    },
+    created() {
+      this._currentForm()
+    },
+    mounted() {
+    },
+    methods: {
+      _currentForm() {
+        if (!isEmptyObject(this.searchForm)) {
+          this.form = this.searchForm
+        }
+      },
+      // 提交查询
+      onSubmitClick() {
+        this._currentForm()
+        this.$refs.searchForm.validate((valid) => {
+          if (valid) {
+//            todo: 增加查询组成json串
+            let requestDatas = []
+            for (let item in this.form) {
+              const itemVal = this.form[item]
+              if (itemVal.length > 0) {
+                const dateType = typeof itemVal
+                console.log(item, itemVal)
+                let val = null
+                if (dateType === 'string' && itemVal.length) {
+                  val = strToUnicode(itemVal)
+                } else {
+                  val = itemVal
+                }
+                requestDatas.push(`{"%${item}%": "` + val + `"}`)
+              }
+            }
+            if (requestDatas.length > 0) {
+              this.isSearch = true
+              this.$emit(ON_SEARCH, {jsonStr: requestDatas.join(',')})
+            }
+          } else {
+            console.log('ERR')
+          }
+        })
+      },
+      // 重置搜索框
+      onResetClick() {
+        this.$refs.searchForm.resetFields()
+        if (this.isSearch) {
+          this.$emit(ON_SEARCH, {})
+          this.isSearch = false
+        }
+      }
+    },
+    components: {},
+    computed: {},
+    watch: {}
+  }
+
+</script>
+
+<style scoped lang='stylus' type='text/stylus'>
+  @import "~@/assets/stylus/color.styl"
+  .kalix-search
+    margin 5px
+    .kalix-search-hd
+      background-color bg-color_1
+      color txt-color_1
+      line-height 44px
+      padding 0 15px
+      text-align left
+    .kalix-search-bd
+      border 1px solid border-color_1
+      font-size 0
+      padding 5px 15px
+      text-align left
+      .search-container
+        display flex
+
+    .el-button
+      .iconfont
+        font-size 14px
+</style>
---
+++ src/views/admin/user/userSearchBak.vue
@@ -0,0 +1,54 @@
+<template lang="pug">
+  kailx-search(title="用户查询" v-bind:search-form="formModel" v-on:onSearch="onSearch" v-bind:form-rules="rules")
+    div.el-form(slot="searchFormSlot")
+      el-form-item(label="bak-登录名" prop="loginName")
+        el-input(v-model="formModel.loginName")
+      el-form-item(label="bak-姓名" prop="loginName")
+        el-input(v-model="formModel.name")
+</template>
+<script type="text/ecmascript-6">
+  import BaseSearch from './baseSearch.vue'
+  import {isEmptyObject} from 'common/util'
+
+  export default {
+    data() {
+      return {
+        formModel: {
+          loginName: '',
+          name: ''
+        },
+        filters: [
+          {label: '登录名', prop: 'loginName'},
+          {label: '姓名', prop: 'name'}
+        ],
+        rules: {
+          loginName: [
+            {message: '请输入登录名', trigger: 'blur'}
+          ],
+          name: [
+            {message: '请输入姓名', trigger: 'blur'}
+          ]
+        }
+      }
+    },
+    created() {
+      this.tempFormModel = JSON.stringify(Object.assign({}, this.formModel))
+    },
+    components: {
+      KailxSearch: BaseSearch
+    },
+    methods: {
+      resetFromModel() {
+        this.formModel = JSON.parse(this.tempFormModel)
+      },
+      onSearch(_requestData) {
+        this.$emit('onSearch', _requestData)
+        if (isEmptyObject(_requestData)) {
+          this.resetFromModel()
+        }
+      }
+    }
+  }
+</script>
+<style scoped lang="stylus" type="text/stylus">
+</style>
