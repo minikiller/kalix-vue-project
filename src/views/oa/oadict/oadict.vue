5d02a5568201127d5345d84871e5fdbf5f06cc57
sunlingfeng <minikiller@qq.com>
2017-08-22 17:49:00 +0800

    增加admin，oa数据字典管理

---
+++ src/views/oa/oadict/oadict.vue
@@ -0,0 +1,53 @@
+<!--
+描述：我的首页-数据字典组件
+开发人：sunlf
+开发日期：2017年8月17日
+-->
+
+<template lang="pug">
+  keep-alive
+    kalix-base-dict(ref="baseTableRef" v-bind:bizKey="bizKey" v-bind:bizDialog="bizDialog" v-bind:dictComponent="dictComponent"
+    v-bind:targetURL="targetURL" v-bind:dialogOptions="dialogOptions")
+</template>
+
+<script type="text/ecmascript-6">
+  import BaseDict from '@/components/biz/dict/basedict'
+  //  import Vue from 'vue'
+  import {OaDictURL, OaDictComponent, OaDictsTypesListURL} from '../config.toml'
+
+  export default {
+    data() {
+      return {
+        dialogOptions: {
+          targetURL: OaDictURL,
+          dictTypesListURL: OaDictsTypesListURL,
+          bizKey: 'dict'
+        },
+        bizKey: 'dict',
+        dictComponent: OaDictComponent,
+        targetURL: OaDictURL,
+        bizDialog: [
+          {id: 'view', dialog: 'basedictView'},
+          {id: 'edit', dialog: 'basedictAdd'},
+          {id: 'add', dialog: 'basedictAdd'}
+        ]
+      }
+    },
+    created() {
+//      this.tempFormModel = JSON.stringify(Object.assign({}, this.formModel))
+//      this.dictComponent.forEach((item) => {
+//        console.log(`[kalix]-[${this.bizKey}] registry name is:  ${item.name}, registry path is: ${item.path}`)
+//        Vue.component(item.name, require('' + item.path))
+//      })
+    },
+    methods: {},
+    components: {
+      KalixBaseDict: BaseDict
+//      KalixUserAdd: UserAdd
+    }
+  }
+</script>
+
+<style scoped lang="stylus">
+
+</style>
