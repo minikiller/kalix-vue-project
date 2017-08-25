050dabcc22cc232cc4cf144ee3d758eb978cf3d9
juezhan <11991762@qq.com>
2017-08-25 10:36:44 +0800

    update baseTable 增加传入列表数据重构函数，绑定对应的键值名称

--- src/views/research/teacher/DataRestructure.js
+++ src/views/research/teacher/DataRestructure.js
@@ -1,22 +1,11 @@
-import Cache from 'common/cache'
-import {isEmptyObject} from 'common/util'
+import {dictKeyObject} from 'common/keyValueObject'
 
 export default function dataRestructure(_data) {
-  let _keyObj = CreateDictKeyObject(`RESEARCH-DICT-KEY`, 'value', 'label')
-  if (_keyObj) {
-    _data.forEach(function (e) {
+  let _keyObj = dictKeyObject(`RESEARCH-DICT-KEY`, 'value', 'label')
+  _data.forEach(function (e) {
+    if (_keyObj) {
       e.positionalTitles = _keyObj[e.positionalTitles]
-    })
-  }
-  return _data
-}
-
-//  获取键值对对象
-function CreateDictKeyObject(_DictKey, _value, _label) {
-  let dataDictKey = JSON.parse(Cache.get(_DictKey))
-  let _keyObj = {}
-  dataDictKey.forEach(function (e) {
-    _keyObj[e[_value]] = e[_label]
+    }
   })
-  return !isEmptyObject(_keyObj) ? _keyObj : null
+  return _data
 }
