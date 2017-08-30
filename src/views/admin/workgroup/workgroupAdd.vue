955fd385a6425cf0d1dc1890c2df970de9bfd157
zangyanming <zyanming2002@163.com>
2017-08-30 16:31:21 +0800

    新增工作组管理

---
+++ src/views/admin/workgroup/workgroupAdd.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+  kalix-dialog.workgroup-add(bizKey="workGroup"
+  ref="kalixBizDialog" v-bind:form-model="formModel" v-bind:targetURL="targetURL")
+    div.el-form(slot="dialogFormSlot")
+      el-form-item(label="组名称" prop="name" v-bind:rules="rules.name")
+        el-input(v-model="formModel.name")
+</template>
+
+<script>
+  import Dialog from '@/components/custom/baseDialog.vue'
+  import { workgroupURL } from '../config.toml'
+
+  export default {
+    props: {
+      formModel: {
+        type: Object,
+        required: true
+      },
+      formRules: {
+        type: Object,
+        required: true
+      }
+    },
+    data() {
+      return {
+        rules: {
+          name: [{required: true, message: '请输入 name', trigger: 'blur'}]
+        },
+        targetURL: workgroupURL
+      }
+    },
+    created() {
+      console.log('this.formRules.name:', this.formRules.name)
+    },
+    components: {
+      KalixDialog: Dialog
+    },
+    methods: {}
+  }
+</script>
+
+<style scoped>
+
+</style>
