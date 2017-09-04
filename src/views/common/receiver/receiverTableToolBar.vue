8cc4687ee072a7959ad64bfd279df3504b483693
juezhan <11991762@qq.com>
2017-09-04 16:39:27 +0800

    add receiverTableToolBar.vue 增加 批量删除 按钮栏

---
+++ src/views/common/receiver/receiverTableToolBar.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  kalix-tool-bar
+    template(slot="toolBarSlot")
+      el-button(v-on:click="onDeleteCheckedClick" type="primary")
+        i.iconfont.icon-delete
+        | 批量删除
+</template>
+<script type="text/ecmascript-6">
+  import EventBus from 'common/eventbus'
+  import ToolBar from '@/components/custom/baseToolBar'
+
+  export default {
+    methods: {
+      onDeleteCheckedClick() {
+        EventBus.$emit('deleteCheckedClick')
+      }
+    },
+    components: {
+      KalixToolBar: ToolBar
+    }
+  }
+</script>
+<style scoped lang="stylus" type="text/stylus">
+</style>
