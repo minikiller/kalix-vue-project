d2060cb55d483251a85cf879361ccf838d023040
sunlingfeng <minikiller@qq.com>
2017-09-12 16:24:17 +0800

    change to cheap-source-map

--- src/views/research/worker/workerAdd.vue
+++ src/views/research/worker/workerAdd.vue
@@ -10,53 +10,52 @@
   )
     div.el-form(slot="dialogFormSlot")
       div.s-flex
-        el-form-item.s-flex_item(label="姓名" prop="name" v-bind:rules="rules.name" label-width="200px")
+        el-form-item.s-flex_item(label="姓名" prop="name" v-bind:rules="rules.name" label-width="120px")
           kalix-user-select(v-bind:params="params" style="width:100%" v-model="formModel.name" v-bind:multiple="false"
           v-on:userSelected="onUserSelected")
-        el-form-item.s-flex_item(label="身份证号" prop="identificationCard" v-bind:rules="rules.identificationCard" label-width="200px")
+        el-form-item.s-flex_item(label="身份证号" prop="identificationCard" v-bind:rules="rules.identificationCard" label-width="120px")
           el-input(v-model="formModel.identificationCard")
-
       div.s-flex
-        el-form-item.s-flex_item(label="性别" prop="sex" v-bind:rules="rules.sex" label-width="200px")
+        el-form-item.s-flex_item(label="性别" prop="sex" v-bind:rules="rules.sex" label-width="120px")
          el-radio-group(v-model="formModel.sex")
           el-radio(label="男")
           el-radio(label="女")
-        el-form-item.s-flex_item(label="籍贯" prop="placeOfOrigin" label-width="200px")
+        el-form-item.s-flex_item(label="籍贯" prop="placeOfOrigin" label-width="120px")
          el-input(v-model="formModel.placeOfOrigin")
       div.s-flex
-        el-form-item.s-flex_item(label="民族" prop="nation" label-width="200px")
+        el-form-item.s-flex_item(label="民族" prop="nation" label-width="120px")
          el-input(v-model="formModel.nation")
-        el-form-item.s-flex_item(label="电子邮件" prop="email" label-width="200px")
+        el-form-item.s-flex_item(label="电子邮件" prop="email" label-width="120px")
          el-input(v-model="formModel.email")
       div.s-flex
-        el-form-item.s-flex_item(label="年龄" prop="age" label-width="200px")
+        el-form-item.s-flex_item(label="年龄" prop="age" label-width="120px")
          el-input(v-model="formModel.age")
-        el-form-item.s-flex_item(label="联系电话" prop="phone" label-width="200px")
+        el-form-item.s-flex_item(label="联系电话" prop="phone" label-width="120px")
          el-input(v-model="formModel.phone")
       div.s-flex
-        el-form-item.s-flex_item(label="最初职称" prop="firstTitle" label-width="200px")
+        el-form-item.s-flex_item(label="最初职称" prop="firstTitle" label-width="120px")
          el-input(v-model="formModel.firstTitle")
-        el-form-item.s-flex_item(label="最初职称评定时间" prop="firstEvaluateDate" label-width="200px")
+        el-form-item.s-flex_item(label="最初评定时间" prop="firstEvaluateDate" label-width="120px")
          el-input(v-model="formModel.firstEvaluateDate")
       div.s-flex
-        el-form-item.s-flex_item(label="最后职称" prop="lastTitle" label-width="200px")
+        el-form-item.s-flex_item(label="最后职称" prop="lastTitle" label-width="120px")
          el-input(v-model="formModel.lastTitle")
-        el-form-item.s-flex_item(label="最后职称评定时间" prop="lastEvaluateDate" label-width="200px")
+        el-form-item.s-flex_item(label="最后评定时间" prop="lastEvaluateDate" label-width="120px")
          el-input(v-model="formModel.lastEvaluateDate")
       div.s-flex
-        el-form-item.s-flex_item(label="最后学历" prop="education" label-width="200px")
+        el-form-item.s-flex_item(label="最后学历" prop="education" label-width="120px")
          el-input(v-model="formModel.education")
-        el-form-item.s-flex_item(label="最后学位" prop="degree" label-width="200px")
+        el-form-item.s-flex_item(label="最后学位" prop="degree" label-width="120px")
          el-input(v-model="formModel.degree")
       div.s-flex
-        el-form-item.s-flex_item(label="毕业院校" prop="school" label-width="200px")
+        el-form-item.s-flex_item(label="毕业院校" prop="school" label-width="120px")
          el-input(v-model="formModel.school")
-        el-form-item.s-flex_item(label="研究方向" prop="direction" label-width="200px")
+        el-form-item.s-flex_item(label="研究方向" prop="direction" label-width="120px")
          el-input(v-model="formModel.direction")
       div.s-flex
-        el-form-item.s-flex_item(label="个人简历" prop="resume" label-width="200px")
+        el-form-item.s-flex_item(label="个人简历" prop="resume" label-width="120px")
          el-input(type="textarea" v-model="formModel.resume")
-        el-form-item.s-flex_item(label="个人说明" prop="introduction" label-width="200px")
+        el-form-item.s-flex_item(label="个人说明" prop="introduction" label-width="120px")
          el-input(type="textarea" v-model="formModel.introduction")
 </template>
 
