a22103a7368f8348dc605a2bfb886bcf87966afb
fengjing <739722894@qq.com>
2017-09-15 16:43:41 +0800

    修改”报名“模块增删改查功能，调整新增对话框样式

--- src/views/research/signup/signupAdd.vue
+++ src/views/research/signup/signupAdd.vue
@@ -8,140 +8,113 @@
   kalix-dialog.user-add(bizKey="signup"
   ref="kalixBizDialog" v-bind:form-model="formModel" v-bind:targetURL="targetURL"
   )
-    div.el-form(slot="dialogFormSlot")
+    div.el-form.kalix-form-table(slot="dialogFormSlot")
+
       div.s-flex
-        el-form-item.s-flex_item(label="展赛名称" prop="competitionId" label-width="140px" )
-          kalix-competition-select(style="width:100%" v-model="formModel.competitionId" v-bind:multiple="false"
-          v-on:competitionSelected="onCompetitionSelected")
-      div.s-flex
-        el-form-item.s-flex_item(label="姓名" prop="name" label-width="140px")
+       el-form-item.s-flex_item.kalix-form-table-td(label="姓名" prop="name" label-width="140px")
           el-input(v-model="formModel.name")
-        div.dd
-        el-form-item.s-flex_item(label="拼音" prop="phoneticize" label-width="140px")
+       el-form-item.s-flex_item.kalix-form-table-td(label="拼音" prop="phoneticize" label-width="140px")
           el-input(v-model="formModel.phoneticize")
-        div.dd
-        el-form-item.s-flex_item(label="笔名" prop="penName" label-width="140px")
+       el-form-item.s-flex_item.kalix-form-table-td(label="笔名" prop="penName" label-width="140px")
           el-input(v-model="formModel.penName")
       div.s-flex
-        el-form-item.s-flex_item(label="国籍" prop="nationality" label-width="140px")
-          el-input(v-model="formModel.nationality")
-        div.dd
-        el-form-item.s-flex_item(label="年龄" prop="age" label-width="140px")
-          el-input(v-model="formModel.age")
-        div.dd
-        el-form-item.s-flex_item(label="性别" prop="sex" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="展赛" prop="competitionId" label-width="140px" )
+          kalix-competition-select(style="width:100%"  v-model="formModel.competitionId" v-bind:multiple="false"
+          v-on:competitionSelected="onCompetitionSelected")
+        el-form-item.s-flex_item.kalix-form-table-td(label="性别" prop="sex" label-width="140px")
           el-radio-group(v-model="formModel.sex")
             el-radio(label="男")
             el-radio(label="女")
+        el-form-item.s-flex_item.kalix-form-table-td(label="年龄" prop="age" label-width="140px")
+          el-input(v-model="formModel.age")
       div.s-flex
-        el-form-item.s-flex_item(label="作者简介" prop="authorIntroduction" label-width="140px")
-          el-input(v-model="formModel.authorIntroduction")
-      div.s-flex
-        el-form-item.s-flex_item(label="省" prop="province" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="省份" prop="province" label-width="140px")
           el-input(v-model="formModel.province")
-        div.dd
-        el-form-item.s-flex_item(label="城市" prop="city" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="城市" prop="city" label-width="140px")
           el-input(v-model="formModel.city")
-        div.dd
-        el-form-item.s-flex_item(label="通讯地址" prop="postalAddress" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="地址" prop="postalAddress" label-width="140px")
           el-input(v-model="formModel.postalAddress")
       div.s-flex
-        el-form-item.s-flex_item(label="出生年月" prop="birthday" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="出生年月" prop="birthday" label-width="140px")
           el-input(v-model="formModel.birthday")
-        div.dd
-        el-form-item.s-flex_item(label="身份证号" prop="identificationCard" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="身份证号" prop="identificationCard" label-width="140px")
           el-input(v-model="formModel.identificationCard")
+        el-form-item.s-flex_item.kalix-form-table-td(label="国籍" prop="nationality" label-width="140px")
+          el-input(v-model="formModel.nationality")
       div.s-flex
-
-      div.s-flex
-
-        div.dd
-        el-form-item.s-flex_item(label="邮政编码" prop="postalCode" label-width="140px")
-          el-input(v-model="formModel.postalCode")
-      div.s-flex
-        el-form-item.s-flex_item(label="移动电话" prop="mobilePhone" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="移动电话" prop="mobilePhone" label-width="140px")
           el-input(v-model="formModel.mobilePhone")
-        div.dd
-        el-form-item.s-flex_item(label="固定电话" prop="fixedPhone" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="固定电话" prop="fixedPhone" label-width="140px")
           el-input(v-model="formModel.fixedPhone")
-      div.s-flex
-        el-form-item.s-flex_item(label="电子邮箱" prop="email" label-width="140px")
-          el-input(v-model="formModel.email")
-        div.dd
-        el-form-item.s-flex_item(label="QQ" prop="qq" label-width="140px")
-          el-input(v-model="formModel.qq")
-      div.s-flex
-        el-form-item.s-flex_item(label="传真" prop="fax" label-width="140px")
-          el-input(v-model="formModel.fax")
-        div.dd
-        el-form-item.s-flex_item(label="微博" prop="microBlog" label-width="140px")
-          el-input(v-model="formModel.microBlog")
-      div.s-flex
-        el-form-item.s-flex_item(label="其他联系方式" prop="otherContactWay" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="其他联系方式" prop="otherContactWay" label-width="140px")
           el-input(v-model="formModel.otherContactWay")
-        div.dd
-        el-form-item.s-flex_item(label="所在学校" prop="school" label-width="140px")
-          el-input(v-model="formModel.school")
       div.s-flex
-        el-form-item.s-flex_item(label="专业/班级" prop="majorclass" label-width="140px")
-          el-input(v-model="formModel.majorclass")
-        div.dd
-        el-form-item.s-flex_item(label="网址" prop="url" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="电子邮箱" prop="email" label-width="140px")
+          el-input(v-model="formModel.email")
+        el-form-item.s-flex_item.kalix-form-table-td(label="邮政编码" prop="postalCode" label-width="140px")
+          el-input(v-model="formModel.postalCode")
+        el-form-item.s-flex_item.kalix-form-table-td(label="网址" prop="url" label-width="140px")
           el-input(v-model="formModel.url")
       div.s-flex
-        el-form-item.s-flex_item(label="团队中文名称" prop="teamCnName" label-width="140px")
-          el-input(v-model="formModel.teamCnName")
-        div.dd
-        el-form-item.s-flex_item(label="团队英文名称" prop="teamEnName" label-width="140px")
-          el-input(v-model="formModel.teamEnName")
+        el-form-item.s-flex_item.kalix-form-table-td(label="QQ" prop="qq" label-width="140px")
+          el-input(v-model="formModel.qq")
+        el-form-item.s-flex_item.kalix-form-table-td(label="传真" prop="fax" label-width="140px")
+          el-input(v-model="formModel.fax")
+        el-form-item.s-flex_item.kalix-form-table-td(label="微博" prop="microBlog" label-width="140px")
+          el-input(v-model="formModel.microBlog")
       div.s-flex
-        el-form-item.s-flex_item(label="团队联系人" prop="teamContacter" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="所在学校" prop="school" label-width="140px")
+          el-input(v-model="formModel.school")
+        el-form-item.s-flex_item.kalix-form-table-td(label="专业/班级" prop="majorclass" label-width="140px")
+          el-input(v-model="formModel.majorclass")
+        el-form-item.s-flex_item.kalix-form-table-td(label="指导教师" prop="tutor" label-width="140px")
+          el-input(v-model="formModel.tutor")
+      div.s-flex
+        el-form-item.s-flex_item.kalix-form-table-td(label="作者简介" prop="authorIntroduction" label-width="140px")
+          el-input(v-model="formModel.authorIntroduction")
+      div.s-flex
+        el-form-item.s-flex_item.kalix-form-table-td(label="团队中文名称" prop="teamCnName" label-width="140px")
+          el-input(v-model="formModel.teamCnName")
+        el-form-item.s-flex_item.kalix-form-table-td(label="团队英文名称" prop="teamEnName" label-width="140px")
+          el-input(v-model="formModel.teamEnName")
+        el-form-item.s-flex_item.kalix-form-table-td(label="团队联系人" prop="teamContacter" label-width="140px")
           el-input(v-model="formModel.teamContacter")
-        div.dd
-        el-form-item.s-flex_item(label="团队所在地" prop="teamAddress" label-width="140px")
+      div.s-flex
+        el-form-item.s-flex_item.kalix-form-table-td(label="主创人员" prop="creator" label-width="140px")
+          el-input(v-model="formModel.creator")
+        el-form-item.s-flex_item.kalix-form-table-td(label="版权所有者" prop="copyrightOwner" label-width="140px")
+          el-input(v-model="formModel.copyrightOwner")
+        el-form-item.s-flex_item.kalix-form-table-td(label="团队所在地" prop="teamAddress" label-width="140px")
           el-input(v-model="formModel.teamAddress")
       div.s-flex
-        el-form-item.s-flex_item(label="作品中文名称" prop="worksCnName" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="作品中文名称" prop="worksCnName" label-width="140px")
           el-input(v-model="formModel.worksCnName")
-        div.dd
-        el-form-item.s-flex_item(label="作品英文名称" prop="worksEnName" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="作品英文名称" prop="worksEnName" label-width="140px")
           el-input(v-model="formModel.worksEnName")
-      div.s-flex
-        el-form-item.s-flex_item(label="作品介绍" prop="worksIntroduction" label-width="140px")
-          el-input(v-model="formModel.worksIntroduction")
-        div.dd
-        el-form-item.s-flex_item(label="出品国家" prop="country" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="作品出品国家" prop="country" label-width="140px")
           el-input(v-model="formModel.country")
       div.s-flex
-        el-form-item.s-flex_item(label="获奖情况" prop="award" label-width="140px")
-          el-input(v-model="formModel.award")
-        div.dd
-        el-form-item.s-flex_item(label="播出情况" prop="broadcasting" label-width="140px")
-          el-input(v-model="formModel.broadcasting")
+        el-form-item.s-flex_item.kalix-form-table-td(label="作品介绍" prop="worksIntroduction" label-width="140px")
+          el-input(v-model="formModel.worksIntroduction")
       div.s-flex
-        el-form-item.s-flex_item(label="指导教师" prop="tutor" label-width="140px")
-          el-input(v-model="formModel.tutor")
-        div.dd
-        el-form-item.s-flex_item(label="制作完成时间" prop="finishingDate" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="获奖情况" prop="award" label-width="140px")
+          el-input(v-model="formModel.award")
+        el-form-item.s-flex_item.kalix-form-table-td(label="播出情况" prop="broadcasting" label-width="140px")
+          el-input(v-model="formModel.broadcasting")
+        el-form-item.s-flex_item.kalix-form-table-td(label="制作完成时间" prop="finishingDate" label-width="140px")
           el-input(v-model="formModel.finishingDate")
       div.s-flex
-        el-form-item.s-flex_item(label="作品时长" prop="lengthOfWork" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="作品时长" prop="lengthOfWork" label-width="140px")
           el-input(v-model="formModel.lengthOfWork")
-        div.dd
-        el-form-item.s-flex_item(label="版权所有者" prop="copyrightOwner" label-width="140px")
-          el-input(v-model="formModel.copyrightOwner")
+
       div.s-flex
-        el-form-item.s-flex_item(label="作者签名" prop="authorSignature" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="作者签名" prop="authorSignature" label-width="140px")
           el-input(v-model="formModel.authorSignature")
-        div.dd
-        el-form-item.s-flex_item(label="出品单位" prop="unit" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="出品单位" prop="unit" label-width="140px")
           el-input(v-model="formModel.unit")
-      div.s-flex
-        el-form-item.s-flex_item(label="出品单位联系人" prop="unitContacter" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="出品单位联系人" prop="unitContacter" label-width="140px")
           el-input(v-model="formModel.unitContacter")
-        div.dd
-        el-form-item.s-flex_item(label="主创人员" prop="creator" label-width="140px")
-          el-input(v-model="formModel.creator")
+
 
  </template>
 
@@ -186,6 +159,11 @@
   }
 </script>
 <style scoped lang="stylus">
+  @import "~@/assets/stylus/color.styl"
   .dd
     width 50px
+  .line
+    height 1px
+    background-color br_color3
+    margin-bottom 20px
 </style>
--- src/views/research/signup/signupView.vue
+++ src/views/research/signup/signupView.vue
@@ -7,139 +7,118 @@
   kalix-dialog.user-add(bizKey="signup"
   ref="kalixBizDialog" v-bind:form-model="formModel" v-bind:targetURL="targetURL"
   v-bind:isView="readonly")
-    div.el-form(slot="dialogFormSlot")
+    div.el-form.kalix-form-table(slot="dialogFormSlot")
       div.s-flex
-        el-form-item.s-flex_item(label="展赛名称" prop="competitionId" v-bind:rules="rules.competitionId" label-width="140px" )
-          kalix-user-select(v-bind:params="params" style="width:100%" v-model="formModel.name" v-bind:multiple="false"
-          v-on:userSelected="onUserSelected")
-      div.s-flex
-        el-form-item.s-flex_item(label="姓名" prop="name" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="姓名" prop="name" label-width="140px")
           el-input(v-model="formModel.name" v-bind:readonly="readonly")
-        div.dd
-        el-form-item.s-flex_item(label="拼音" prop="phoneticize" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="拼音" prop="phoneticize" label-width="140px")
           el-input(v-model="formModel.phoneticize" v-bind:readonly="readonly")
-      div.s-flex
-        el-form-item.s-flex_item(label="笔名" prop="penName" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="笔名" prop="penName" label-width="140px")
           el-input(v-model="formModel.penName" v-bind:readonly="readonly")
-        div.dd
-        el-form-item.s-flex_item(label="性别" prop="sex" label-width="140px")
-          el-input(v-model="formModel.sex" v-bind:readonly="readonly")
       div.s-flex
-        el-form-item.s-flex_item(label="作者简介" prop="authorIntroduction" label-width="140px")
-          el-input(v-model="formModel.authorIntroduction" v-bind:readonly="readonly")
-      div.s-flex
-        el-form-item.s-flex_item(label="国籍" prop="nationality" label-width="140px")
-          el-input(v-model="formModel.nationality" v-bind:readonly="readonly")
-        div.dd
-        el-form-item.s-flex_item(label="年龄" prop="age" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="展赛" prop="competitionId" label-width="140px" )
+          kalix-competition-select(style="width:100%"  v-model="formModel.competitionId" v-bind:multiple="false"
+          v-on:competitionSelected="onCompetitionSelected" v-bind:readonly="readonly")
+        el-form-item.s-flex_item.kalix-form-table-td(label="性别" prop="sex" label-width="140px")
+          el-radio-group(v-model="formModel.sex" v-bind:readonly="readonly")
+            el-radio(label="男")
+            el-radio(label="女")
+        el-form-item.s-flex_item.kalix-form-table-td(label="年龄" prop="age" label-width="140px")
           el-input(v-model="formModel.age" v-bind:readonly="readonly")
       div.s-flex
-        el-form-item.s-flex_item(label="出生年月" prop="birthday" label-width="140px")
-          el-input(v-model="formModel.birthday" v-bind:readonly="readonly")
-        div.dd
-        el-form-item.s-flex_item(label="身份证号" prop="identificationCard" label-width="140px")
-          el-input(v-model="formModel.identificationCard" v-bind:readonly="readonly")
-      div.s-flex
-        el-form-item.s-flex_item(label="省" prop="province" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="省份" prop="province" label-width="140px")
           el-input(v-model="formModel.province" v-bind:readonly="readonly")
-        div.dd
-        el-form-item.s-flex_item(label="城市" prop="city" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="城市" prop="city" label-width="140px")
           el-input(v-model="formModel.city" v-bind:readonly="readonly")
-      div.s-flex
-        el-form-item.s-flex_item(label="通讯地址" prop="postalAddress" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="地址" prop="postalAddress" label-width="140px")
           el-input(v-model="formModel.postalAddress" v-bind:readonly="readonly")
-        div.dd
-        el-form-item.s-flex_item(label="邮政编码" prop="postalCode" label-width="140px")
-          el-input(v-model="formModel.postalCode" v-bind:readonly="readonly")
       div.s-flex
-        el-form-item.s-flex_item(label="移动电话" prop="mobilePhone" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="出生年月" prop="birthday" label-width="140px")
+          el-input(v-model="formModel.birthday" v-bind:readonly="readonly")
+        el-form-item.s-flex_item.kalix-form-table-td(label="身份证号" prop="identificationCard" label-width="140px")
+          el-input(v-model="formModel.identificationCard" v-bind:readonly="readonly")
+        el-form-item.s-flex_item.kalix-form-table-td(label="国籍" prop="nationality" label-width="140px")
+          el-input(v-model="formModel.nationality" v-bind:readonly="readonly")
+      div.s-flex
+        el-form-item.s-flex_item.kalix-form-table-td(label="移动电话" prop="mobilePhone" label-width="140px")
           el-input(v-model="formModel.mobilePhone" v-bind:readonly="readonly")
-        div.dd
-        el-form-item.s-flex_item(label="固定电话" prop="fixedPhone" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="固定电话" prop="fixedPhone" label-width="140px")
           el-input(v-model="formModel.fixedPhone" v-bind:readonly="readonly")
-      div.s-flex
-        el-form-item.s-flex_item(label="电子邮箱" prop="email" label-width="140px")
-          el-input(v-model="formModel.email" v-bind:readonly="readonly")
-        div.dd
-        el-form-item.s-flex_item(label="QQ" prop="qq" label-width="140px")
-          el-input(v-model="formModel.qq" v-bind:readonly="readonly")
-      div.s-flex
-        el-form-item.s-flex_item(label="传真" prop="fax" label-width="140px")
-          el-input(v-model="formModel.fax" v-bind:readonly="readonly")
-        div.dd
-        el-form-item.s-flex_item(label="微博" prop="microBlog" label-width="140px")
-          el-input(v-model="formModel.microBlog" v-bind:readonly="readonly")
-      div.s-flex
-        el-form-item.s-flex_item(label="其他联系方式" prop="otherContactWay" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="其他联系方式" prop="otherContactWay" label-width="140px")
           el-input(v-model="formModel.otherContactWay" v-bind:readonly="readonly")
-        div.dd
-        el-form-item.s-flex_item(label="所在学校" prop="school" label-width="140px")
-          el-input(v-model="formModel.school" v-bind:readonly="readonly")
       div.s-flex
-        el-form-item.s-flex_item(label="专业/班级" prop="majorclass" label-width="140px")
-          el-input(v-model="formModel.majorclass" v-bind:readonly="readonly")
-        div.dd
-        el-form-item.s-flex_item(label="网址" prop="url" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="电子邮箱" prop="email" label-width="140px")
+          el-input(v-model="formModel.email" v-bind:readonly="readonly")
+        el-form-item.s-flex_item.kalix-form-table-td(label="邮政编码" prop="postalCode" label-width="140px")
+          el-input(v-model="formModel.postalCode" v-bind:readonly="readonly")
+        el-form-item.s-flex_item.kalix-form-table-td(label="网址" prop="url" label-width="140px")
           el-input(v-model="formModel.url" v-bind:readonly="readonly")
       div.s-flex
-        el-form-item.s-flex_item(label="团队中文名称" prop="teamCnName" label-width="140px")
-          el-input(v-model="formModel.teamCnName" v-bind:readonly="readonly")
-        div.dd
-        el-form-item.s-flex_item(label="团队英文名称" prop="teamEnName" label-width="140px")
-          el-input(v-model="formModel.teamEnName" v-bind:readonly="readonly")
+        el-form-item.s-flex_item.kalix-form-table-td(label="QQ" prop="qq" label-width="140px")
+          el-input(v-model="formModel.qq" v-bind:readonly="readonly")
+        el-form-item.s-flex_item.kalix-form-table-td(label="传真" prop="fax" label-width="140px")
+          el-input(v-model="formModel.fax" v-bind:readonly="readonly")
+        el-form-item.s-flex_item.kalix-form-table-td(label="微博" prop="microBlog" label-width="140px")
+          el-input(v-model="formModel.microBlog" v-bind:readonly="readonly")
       div.s-flex
-        el-form-item.s-flex_item(label="团队联系人" prop="teamContacter" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="所在学校" prop="school" label-width="140px")
+          el-input(v-model="formModel.school" v-bind:readonly="readonly")
+        el-form-item.s-flex_item.kalix-form-table-td(label="专业/班级" prop="majorclass" label-width="140px")
+          el-input(v-model="formModel.majorclass" v-bind:readonly="readonly")
+        el-form-item.s-flex_item.kalix-form-table-td(label="指导教师" prop="tutor" label-width="140px")
+          el-input(v-model="formModel.tutor" v-bind:readonly="readonly")
+      div.s-flex
+        el-form-item.s-flex_item.kalix-form-table-td(label="作者简介" prop="authorIntroduction" label-width="140px")
+          el-input(v-model="formModel.authorIntroduction" v-bind:readonly="readonly")
+      div.s-flex
+        el-form-item.s-flex_item.kalix-form-table-td(label="团队中文名称" prop="teamCnName" label-width="140px")
+          el-input(v-model="formModel.teamCnName" v-bind:readonly="readonly")
+        el-form-item.s-flex_item.kalix-form-table-td(label="团队英文名称" prop="teamEnName" label-width="140px")
+          el-input(v-model="formModel.teamEnName" v-bind:readonly="readonly")
+        el-form-item.s-flex_item.kalix-form-table-td(label="团队联系人" prop="teamContacter" label-width="140px")
           el-input(v-model="formModel.teamContacter" v-bind:readonly="readonly")
-        div.dd
-        el-form-item.s-flex_item(label="团队所在地" prop="teamAddress" label-width="140px")
+      div.s-flex
+        el-form-item.s-flex_item.kalix-form-table-td(label="主创人员" prop="creator" label-width="140px")
+          el-input(v-model="formModel.creator" v-bind:readonly="readonly")
+        el-form-item.s-flex_item.kalix-form-table-td(label="版权所有者" prop="copyrightOwner" label-width="140px")
+          el-input(v-model="formModel.copyrightOwner" v-bind:readonly="readonly")
+        el-form-item.s-flex_item.kalix-form-table-td(label="团队所在地" prop="teamAddress" label-width="140px")
           el-input(v-model="formModel.teamAddress" v-bind:readonly="readonly")
       div.s-flex
-        el-form-item.s-flex_item(label="作品中文名称" prop="worksCnName" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="作品中文名称" prop="worksCnName" label-width="140px")
           el-input(v-model="formModel.worksCnName" v-bind:readonly="readonly")
-        div.dd
-        el-form-item.s-flex_item(label="作品英文名称" prop="worksEnName" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="作品英文名称" prop="worksEnName" label-width="140px")
           el-input(v-model="formModel.worksEnName" v-bind:readonly="readonly")
-      div.s-flex
-        el-form-item.s-flex_item(label="作品介绍" prop="worksIntroduction" label-width="140px")
-          el-input(v-model="formModel.worksIntroduction" v-bind:readonly="readonly")
-        div.dd
-        el-form-item.s-flex_item(label="出品国家" prop="country" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="作品出品国家" prop="country" label-width="140px")
           el-input(v-model="formModel.country" v-bind:readonly="readonly")
       div.s-flex
-        el-form-item.s-flex_item(label="获奖情况" prop="award" label-width="140px")
-          el-input(v-model="formModel.award" v-bind:readonly="readonly")
-        div.dd
-        el-form-item.s-flex_item(label="播出情况" prop="broadcasting" label-width="140px")
-          el-input(v-model="formModel.broadcasting" v-bind:readonly="readonly")
+        el-form-item.s-flex_item.kalix-form-table-td(label="作品介绍" prop="worksIntroduction" label-width="140px")
+          el-input(v-model="formModel.worksIntroduction" v-bind:readonly="readonly")
       div.s-flex
-        el-form-item.s-flex_item(label="指导教师" prop="tutor" label-width="140px")
-          el-input(v-model="formModel.tutor" v-bind:readonly="readonly")
-        div.dd
-        el-form-item.s-flex_item(label="制作完成时间" prop="finishingDate" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="获奖情况" prop="award" label-width="140px")
+          el-input(v-model="formModel.award" v-bind:readonly="readonly")
+        el-form-item.s-flex_item.kalix-form-table-td(label="播出情况" prop="broadcasting" label-width="140px")
+          el-input(v-model="formModel.broadcasting" v-bind:readonly="readonly")
+        el-form-item.s-flex_item.kalix-form-table-td(label="制作完成时间" prop="finishingDate" label-width="140px")
           el-input(v-model="formModel.finishingDate" v-bind:readonly="readonly")
       div.s-flex
-        el-form-item.s-flex_item(label="作品时长" prop="lengthOfWork" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="作品时长" prop="lengthOfWork" label-width="140px")
           el-input(v-model="formModel.lengthOfWork" v-bind:readonly="readonly")
-        div.dd
-        el-form-item.s-flex_item(label="版权所有者" prop="copyrightOwner" label-width="140px")
-          el-input(v-model="formModel.copyrightOwner" v-bind:readonly="readonly")
+
       div.s-flex
-        el-form-item.s-flex_item(label="作者签名" prop="authorSignature" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="作者签名" prop="authorSignature" label-width="140px")
           el-input(v-model="formModel.authorSignature" v-bind:readonly="readonly")
-        div.dd
-        el-form-item.s-flex_item(label="出品单位" prop="unit" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="出品单位" prop="unit" label-width="140px")
           el-input(v-model="formModel.unit" v-bind:readonly="readonly")
-      div.s-flex
-        el-form-item.s-flex_item(label="出品单位联系人" prop="unitContacter" label-width="140px")
+        el-form-item.s-flex_item.kalix-form-table-td(label="出品单位联系人" prop="unitContacter" label-width="140px")
           el-input(v-model="formModel.unitContacter" v-bind:readonly="readonly")
-        div.dd
-        el-form-item.s-flex_item(label="主创人员" prop="creator" label-width="140px")
-          el-input(v-model="formModel.creator" v-bind:readonly="readonly")
+
 </template>
 <script type="text/ecmascript-6">
   import Dialog from '@/components/custom/baseDialog.vue'
   import {CompetitionInfoURL} from '../config.toml'
-
+  import CompetitionSelect from '@/components/biz/competitionselect/competitionselect'
+  import EventBus from 'common/eventbus'
   export default {
     props: {
       formModel: {
@@ -163,9 +142,13 @@
     created() {
     },
     components: {
-      KalixDialog: Dialog
+      KalixDialog: Dialog,
+      KalixCompetitionSelect: CompetitionSelect
     },
     methods: {
+      onCompetitionSelected(competition) {
+        EventBus.$emit('updateSignupModel', competition)
+      }
     }
   }
 </script>
