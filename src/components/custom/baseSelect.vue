2e3dddeb9ee7efe4b5aa210975a73189e7461f7b
juezhan <11991762@qq.com>
2017-09-15 14:29:06 +0800

    add baseSelect.vue

---
+++ src/components/custom/baseSelect.vue
@@ -0,0 +1,46 @@
+<template lang="pug">
+  el-select(v-model="currentValue" v-bind:placeholder="placeholder" v-on:change="onChange")
+    el-option(v-for="item in options"
+    v-bind:key="item.id"
+    v-bind:label="item.name"
+    v-bind:value="item.id")
+</template>
+<script type="text/ecmascript-6">
+  import Cache from 'common/cache'
+
+  export default {
+    props: ['requestUrl', 'placeholder', 'value', 'appName'],
+    data() {
+      return {
+        currentValue: this.value,
+        options: []
+      }
+    },
+    created() {
+      this.initOptions()
+    },
+    methods: {
+      initOptions() {
+        const DictKey = `${this.appName.toUpperCase()}-KEY`
+        if (!Cache.get(DictKey)) {
+          this.$http
+            .get(this.requestUrl, {
+              params: {page: 1, start: 0, limit: 20}
+            })
+            .then(res => {
+              this.options = res.data.data
+              Cache.save(DictKey, JSON.stringify(this.options))
+            })
+        } else {
+          this.options = JSON.parse(Cache.get(DictKey))
+        }
+      },
+      onChange(value) {
+        console.log('[value]', value)
+        this.$emit('input', value)
+      }
+    }
+  }
+</script>
+<style scoped lang="stylus" type="text/stylus">
+</style>
