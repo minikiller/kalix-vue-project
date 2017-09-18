bc0235982c15d3cf2ed47a0b2059e993ec5fb979
sunlingfeng <minikiller@qq.com>
2017-09-19 00:06:23 +0800

    完成table和page一起的pagedTable的封装。

---
+++ src/components/custom/pagedTable.vue
@@ -0,0 +1,74 @@
+<!--
+描述：table 组件的二次封装,带table和page
+开发人：sunlf
+开发日期：2017年9月18日
+-->
+<template lang="pug">
+  div
+    el-table(v-if="tableData && tableData.length > 0" v-bind:data="tableData" stripe style="width:100%" border fit)
+      el-table-column(label="行号" width="70")
+        template(scope="scope")
+          div(style="text-align: center") {{ scope.row.rowNumber }}
+      slot(name="tableColumnSlot")
+    el-pagination.kalix-table-pagination(v-if="this.pager.totalCount"
+    v-on:size-change="pagerSizeChange"
+    v-on:current-change="pagerCurrentChange"
+    v-bind:current-page="pager.currentPage"
+    v-bind:page-sizes="pager.pageSizes"
+    v-bind:page-size="1"
+    layout="total, sizes, prev, pager, next, jumper"
+    v-bind:total="this.pager.totalCount")
+</template>
+<script type="text/ecmascript-6">
+  import {PageConfig} from 'config/global.toml'
+
+  export default {
+    props: ['targetURL'],
+    mounted() {
+      this.getBizData()
+    },
+    computed: {
+      rowNo() {
+        return (1 + ((this.pager.currentPage - 1) * this.pager.limit)) // 返回当前行号
+      }
+    },
+    methods: {
+      getBizData() { // 获得流程历史
+        this.axios.request({
+          method: 'GET',
+          url: this.targetURL,
+          params: {},
+          data: {}
+        }).then((res) => {
+          this.tableData = res.data.data.map((item, index) => {
+            item.rowNumber = index + this.rowNo
+            return item
+          })
+          this.pager.totalCount = res.data.totalCount
+//          Message.processResult(res)
+        })
+      },
+      pagerSizeChange(val) { //  改变每页记录数
+        this.pager.limit = val
+        this._getFilesList()
+      },
+      pagerCurrentChange(val) { //  翻页
+        this.pager.currentPage = val
+        this._getFilesList()
+      }
+    },
+    data() {
+      return {
+        tableData: [],
+        pager: {
+          totalCount: 0,
+          pageSizes: PageConfig.sizes,
+          currentPage: 1,
+          limit: PageConfig.limit,
+          start: 0
+        }
+      }
+    }
+  }
+</script>
+
--- src/views/oa/comp/taskComplete.vue
+++ src/views/oa/comp/taskComplete.vue
@@ -19,35 +19,25 @@
           el-tab-pane(label="业务数据")
             component(:is="whichBizForm" v-bind:form-model="bizForm")
           el-tab-pane(label="流程历史")
-            el-table(:data="taskActivityData" stripe style="width:100%" border fit)
-              el-table-column(label="行号" width="70")
-                template(scope="scope")
-                  div(style="text-align: center") {{ scope.row.rowNumber }}
-              el-table-column(prop="activityName" label="节点名称" align="center" width="200")
-              el-table-column(prop="assignee" label="执行人" align="center")
-              kalix-date-column(prop="startTime" label="开始时间")
-              kalix-date-column(prop="endTime" label="结束时间")
-              el-table-column(prop="durationInMillis" label="持续时长" align="center")
-              el-table-column(prop="result" label="审批结果" align="center")
-              el-table-column(prop="comment" label="审批意见" align="center")
-            el-pagination.kalix-table-pagination(v-if="pager.totalCount"
-            v-on:size-change="pagerSizeChange"
-            v-on:current-change="pagerCurrentChange"
-            v-bind:current-page="pager.currentPage"
-            v-bind:page-sizes="pager.pageSizes"
-            v-bind:page-size="1"
-            layout="total, sizes, prev, pager, next, jumper"
-            v-bind:total="pager.totalCount")
+            kalix-paged-table(v-bind:targetURL="targetURL")
+              template(slot="tableColumnSlot")
+                el-table-column(prop="activityName" label="节点名称" align="center" width="220")
+                el-table-column(prop="assignee" label="执行人" align="center"  width="90")
+                kalix-date-column(prop="startTime" label="开始时间")
+                kalix-date-column(prop="endTime" label="结束时间")
+                el-table-column(prop="durationInMillis" label="持续时长" align="center"  width="220")
+                el-table-column(prop="result" label="审批结果" align="center"  width="220")
+                el-table-column(prop="comment" label="审批意见" align="center"  width="220")
           el-tab-pane(label="附件数据")
 </template>
 
 <script type="text/ecmascript-6">
   import {TaskActivitiesURL, TaskFormURL} from '../config.toml'
-  import Dialog from '@/components/custom/baseDialog.vue'
+  import Dialog from '@/components/custom/baseDialog'
   import EventBus from 'common/eventbus'
   import {ON_INIT_DIALOG_DATA} from '@/components/custom/event.toml'
   import DateColumn from 'views/oa/comp/dateColumn'
-  import {PageConfig} from 'config/global.toml'
+  import PagedTable from '@/components/custom/pagedTable'
   //  import Message from 'common/message'
   const baseFormUrl = '/camel/rest/'
   const _import = require('@/api/_import_' + process.env.NODE_ENV)
@@ -56,6 +46,7 @@
     data() {
       return {
         taskActivityData: [], // 流程历史
+        targetURL: '',
         bizData: {}, // 流程业务的动态返回配置信息
         bizForm: {}, // 流程数据信息
         title: '',
@@ -65,14 +56,8 @@
         formModel: {},
         bizKey: 'taskComplete',
         whichBizForm: '', // 动态加载业务view
-        form: {},
-        pager: {
-          totalCount: 0,
-          pageSizes: PageConfig.sizes,
-          currentPage: 1,
-          limit: PageConfig.limit,
-          start: 0
-        }
+        totalCount: 0,
+        form: {}
       }
     },
     mounted() {
@@ -80,44 +65,17 @@
     },
     components: {
       KalixDialog: Dialog,
-      KalixDateColumn: DateColumn
-    },
-    computed: {
-      rowNo() {
-        return (1 + ((this.pager.currentPage - 1) * this.pager.limit)) // 返回当前行号
-      }
+      KalixDateColumn: DateColumn,
+      KalixPagedTable: PagedTable
     },
     methods: {
-      pagerSizeChange(val) { //  改变每页记录数
-        this.pager.limit = val
-        this._getFilesList()
-      },
-      pagerCurrentChange(val) { //  翻页
-        this.pager.currentPage = val
-        this._getFilesList()
-      },
       onClose() {
         EventBus.$off(this.bizKey + '-' + ON_INIT_DIALOG_DATA)
       },
       initData(row) {
         this.title = '流程审批-' + row.name
-        this.getTaskActivity(row)
+        this.targetURL = TaskActivitiesURL + row.processInstanceId
         this.getBizData(row)
-      },
-      getTaskActivity(row) { // 获得流程历史
-        this.axios.request({
-          method: 'GET',
-          url: TaskActivitiesURL + row.processInstanceId,
-          params: {},
-          data: {}
-        }).then((res) => {
-          this.taskActivityData = res.data.data.map((item, index) => {
-            item.rowNumber = index + this.rowNo
-            return item
-          })
-          this.pager.totalCount = res.data.totalCount
-//          Message.processResult(res)
-        })
       },
       getBizData(row) {
         this.axios.request({
