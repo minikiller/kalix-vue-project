d93e74e1a3ce6772ba8261d04527863e0cf9cfeb
juezhan <11991762@qq.com>
2017-09-18 16:54:53 +0800

    update meetingApplyAdd,userselect,baseSelect,baseTable 修改会议申请新建和编辑。编辑会议纪要人等信息时会删除原有纪要人 有待修复

--- src/views/oa/employapply/employapply.vue
+++ src/views/oa/employapply/employapply.vue
@@ -1,8 +1,30 @@
-<template>
-  <div>
-    employapply
-  </div>
+<template lang="pug">
+  div employapply
+    el-button(v-on:click="clickEdit") 编辑
+    div selectedValue:{{selectedValue}}
+    employ-apply-add(ref="employApplyAdd" v-on:selected="onSelected")
 </template>
 <script type="text/ecmascript-6">
+  import EmployApplyAdd from './employapplyAdd.vue'
+
+  export default {
+    data() {
+      return {
+        selectedValue: []
+      }
+    },
+    methods: {
+      clickEdit() {
+        let obj = this.selectedValue
+        this.$refs.employApplyAdd.open(obj)
+      },
+      onSelected(val) {
+        this.selectedValue = val
+      }
+    },
+    components: {
+      EmployApplyAdd
+    }
+  }
 </script>
 
