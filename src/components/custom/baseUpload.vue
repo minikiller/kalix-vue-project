ae721984129da5f5937b3138503648ee80881556
houqingjie <150091225@qq.com>
2017-09-22 14:32:56 +0800

    完善附件上传组件功能,设置查看时显示样式

--- src/components/custom/baseUpload.vue
+++ src/components/custom/baseUpload.vue
@@ -6,17 +6,23 @@
 
 <template lang="pug">
   div.block
-    el-upload(v-bind:class="{'avatar-uploader':isImage,'upload-demo':!isImage}" v-bind:action="action" v-bind:headers="headers"
-    v-bind:multiple="false" v-bind:show-file-list="true" v-bind:file-list="fileList"
-    v-bind:on-change="handleChange" v-bind:before-upload="handleBeforeUpload"
-    v-bind:on-success="handleSuccess" v-bind:on-error="handleError"
-    v-bind:on-preview="handlePreview" v-bind:on-remove="handleRemove")
+    template(v-if="readonly")
       template(v-if="isImage")
         img.avatar(v-if="imageUrl" v-bind:src="imageUrl")
-        i.el-icon-plus.avatar-uploader-icon(v-else)
+        img.avatar(v-else src="./images/default_attachment.png")
       template(v-else)
-        template(v-if="!readonly")
-          el-button(size="small" type="primary") 点击上传
+        template(v-if="this.value")
+          a(v-bind:href="this.value") {{this.fileName}}
+        template(v-else) 空
+    template(v-else)
+      el-upload(v-bind:class="{'avatar-uploader':isImage,'upload-demo':!isImage}" v-bind:action="action" v-bind:headers="headers"
+      v-bind:multiple="false" v-bind:show-file-list="true" v-bind:file-list="fileList"
+      v-bind:on-change="handleChange" v-bind:before-upload="handleBeforeUpload"
+      v-bind:on-success="handleSuccess" v-bind:on-error="handleError"
+      v-bind:on-preview="handlePreview" v-bind:on-remove="handleRemove")
+        template(v-if="isImage")
+          img.avatar(v-if="imageUrl" v-bind:src="imageUrl")
+          i.el-icon-plus.avatar-uploader-icon(v-else)
         template(v-else)
           el-button(size="small" type="primary") 点击上传
 </template>
@@ -27,7 +33,6 @@
   import Vue from 'vue'
 
   let fileCount = 0
-  // let attachmentId = ''
   export default {
     mounted () {
       this.fentch()
@@ -37,6 +42,7 @@
         action: baseURL + uploadURL,
         headers: {'access_token': Cache.get('access_token'), 'jsessionid': Cache.get('user_token')},
         fileList: [],
+        fileName: '',
         imageUrl: ''
       }
     },
@@ -46,9 +52,8 @@
         if (this.value !== '') {
           let pathParts = this.value.split('/')
           // let fileName = pathParts[pathParts.length - 1]
-          let fileName = pathParts.pop()
-          // attachmentId = pathParts.pop()
-          let obj = {'name': fileName, 'url': this.value}
+          this.fileName = pathParts.pop()
+          let obj = {'name': this.fileName, 'url': this.value}
           this.fileList.push(obj)
 
           if (this.isImage) {
