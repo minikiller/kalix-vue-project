83e5889da06123da57294fe11d53083b6431509b
sunlingfeng <minikiller@qq.com>
2017-09-25 15:34:40 +0800

    新增数据字典的input组件

---
+++ src/views/oa/sealapply/model.js
@@ -0,0 +1,20 @@
+const FormModel = {
+  title: '吉林动画学院印章使用申请单',
+  orgId: '',
+  orgName: '',
+  creationDate: '',
+  usageCount: '',
+  sealType: '公司公章',
+  sealTypeName: '',
+  createBy: '',
+  auditResult: '',
+  currentNode: '',
+  departmentHead: '', // 部门负责人
+  tableFormField: '', // 分公司负责人
+  counsel: '', // 法律顾问
+  generalManager: '', // 总经理
+  sealAdministrator: '', // 印章专管员
+  remark: '' // 备注
+}
+
+export default FormModel
--- src/views/oa/sealapply/sealApplyView.vue
+++ src/views/oa/sealapply/sealApplyView.vue
@@ -1,11 +1,44 @@
+<!--
+描述：人员管理-印章申请-查看组件
+开发人：sunlf
+开发日期：2017年8月17日
+-->
+
 <template lang="pug">
-    div
+  kalix-dialog.user-add(bizKey="sealApply"
+  v-bind:formModel.sync="formModel" v-bind:targetURL="targetURL"
+  ref="kalixBizDialog"
+  v-bind:isView="readonly"
+  )
+    div.el-form.kalix-form-table(slot="dialogFormSlot")
+      kalix-seal-apply-view-form(v-bind:form-model="formModel")
 </template>
 
 <script type="text/ecmascript-6">
-    export default {}
+  import Dialog from '@/components/custom/baseDialog.vue'
+  import BaseDictSelect from '@/components/custom/baseDictSelect'
+  import SealApplyViewForm from './SealApplyViewForm'
+  import FormModel from './model'
+  //  import {usersURL} from 'views/admin/config.toml'
+
+  export default {
+    data() {
+      return {
+        formModel: Object.assign({}, FormModel),
+        targetURL: '',
+        readonly: true
+      }
+    },
+    created() {
+      this.labelWidth = '110px'
+//      this.getMeetingTypeName()
+    },
+    components: {
+      KalixDialog: Dialog,
+      KalixDictSelect: BaseDictSelect,
+      KalixSealApplyViewForm: SealApplyViewForm
+    },
+    methods: {
+    }
+  }
 </script>
-
-<style scoped lang="stylus">
-
-</style>
