6703d13bc0d52cae5bb953d51e2211450fc34188
sunlingfeng <minikiller@qq.com>
2017-09-28 16:03:52 +0800

    修复用户组织下拉列表的显示问题。

--- src/components/biz/select/MeetingRoomSelect.vue
+++ src/components/biz/select/MeetingRoomSelect.vue
@@ -1,12 +1,20 @@
 <template lang="pug">
   kalix-select(v-model="currentValue" placeholder="请选择会议地点" style="width:100%"
-  appName="MEETINGROOMS" v-on:selectChange="onChange")
+  appName="MEETINGROOMS" v-on:selectChange="onChange" v-bind:requestUrl="requestUrl")
 </template>
 <script type="text/ecmascript-6">
   import {SelectMixin} from './SelectMixin'
 
+  const MeetingRoomsURL = '/camel/rest/meetingrooms'
   export default {
-    mixins: [SelectMixin]
+    mixins: [SelectMixin],
+    data() {
+      return {
+        requestUrl: MeetingRoomsURL
+      }
+    },
+    created() {
+    }
   }
 </script>
 <style scoped lang="stylus" type="text/stylus">
--- src/components/biz/select/UserOrgSelect.vue
+++ src/components/biz/select/UserOrgSelect.vue
@@ -1,12 +1,23 @@
 <template lang="pug">
   kalix-select(v-model="currentValue" placeholder="请选择申请部门" style="width:100%"
-  appName="USERORGS" v-on:selectChange="onChange")
+  appName="USERORGS" v-on:selectChange="onChange" v-bind:requestUrl="requestUrl")
 </template>
 <script type="text/ecmascript-6">
   import {SelectMixin} from './SelectMixin'
+  import Cache from 'common/cache'
+
+  const UserOrgsURL = '/camel/rest/users/[usersId]/orgs'
 
   export default {
-    mixins: [SelectMixin]
+    mixins: [SelectMixin],
+    data() {
+      return {
+        requestUrl: UserOrgsURL.replace('[usersId]', Cache.get('id'))
+      }
+    },
+    created() {
+//      this._initDict(`${'UserOrgs'.toUpperCase()}-KEY`, UserOrgsURL.replace('[usersId]', Cache.get('id')))
+    }
   }
 </script>
 <style scoped lang="stylus" type="text/stylus">
