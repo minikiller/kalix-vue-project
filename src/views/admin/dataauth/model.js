4e69c9cb1e246dad5e1fde4952af6183b8091761
fengjing <739722894@qq.com>
2017-10-25 15:35:24 +0800

    增加数据权限管理功能

---
+++ src/views/admin/dataauth/model.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Administrator on 2017/9/22.
+ */
+const FormModel = {
+  type: null,
+  name: '',
+  remark: '',
+  appId: '',
+  appName: '',
+  menuId: '',
+  menuName: ''
+}
+export default FormModel
