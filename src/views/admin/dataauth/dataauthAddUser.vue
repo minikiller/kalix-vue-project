148b1ab4e71b9bb711ff41de0bf35c7ce0ac64bf
fengjing <739722894@qq.com>
2017-10-26 16:53:36 +0800

    新建数据权限选取用户组建

---
+++ src/views/admin/dataauth/dataauthAddUser.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  el-transfer(v-model="value4")
+</template>
+
+<script>
+  export default {
+    data() {
+      const generateData3 = _ => {
+        const data = []
+        for (let i = 1; i <= 15; i++) {
+          data.push({
+            value: i,
+            desc: '',
+            disabled: i % 4 === 0
+          })
+        }
+        return data
+      }
+      return {
+        data3: generateData3(),
+        value4: []
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/admin/dataauth/dataauthView.vue
+++ src/views/admin/dataauth/dataauthView.vue
@@ -6,6 +6,8 @@
         el-input(v-model="formModel.type" v-bind:readonly="readonly")
       el-form-item(label="应用名称" prop="appid"  label-width="140px")
         el-input(v-model="formModel.appid" v-bind:readonly="readonly")
+      el-form-item(label="菜单名称" prop="menuId" label-width="140px")
+        el-input(v-model="formModel.menuId" readonly)
       el-form-item(label="菜单名称" prop="menuid"  label-width="140px")
         el-input(v-model="formModel.menuid" v-bind:readonly="readonly")
 </template>
