061423feaa7e8ffdd84afa9fe88f1756a3aba5f4
fengjing <739722894@qq.com>
2017-10-27 16:53:58 +0800

    新建数据权限选取用户组建

--- src/views/admin/dataauth/dataauthAddUser.vue
+++ src/views/admin/dataauth/dataauthAddUser.vue
@@ -1,29 +1,48 @@
 <template lang="pug">
-  el-transfer(v-model="value4")
+  kalix-dialog.dataauth-addUser(bizKey="dataauth" ref="kalixBizDialog"
+  style="width:1100px;left:20%")
+   div.el-dialog__header(style="#5fa2dd")
+   div.el-form(slot="dialogFormSlot" )
+    el-transfer(v-model="value3" style="width:100%" filterable
+    v-bind:left-default-checked="[2, 3]"
+    v-bind:right-default-checked="[1]"
+    v-bind:titles="['可选项', '已选项']"
+    v-bind:footer-format="{noChecked: '${total}', hasChecked: '${checked}/${total}'}"
+    v-bind:data="data2")
 </template>
 
 <script>
+  import Dialog from '@/components/custom/baseDialog.vue'
   export default {
     data() {
-      const generateData3 = _ => {
+      const generateData = _ => {
+        console('11111222222233333333')
+        const DictKey = '$USER-KEY'
+        console('DictKey', DictKey)
         const data = []
         for (let i = 1; i <= 15; i++) {
           data.push({
-            value: i,
-            desc: '',
+            key: i,
+            label: '备选项',
             disabled: i % 4 === 0
           })
         }
         return data
       }
       return {
-        data3: generateData3(),
-        value4: []
+        data2: generateData(),
+        value3: [1]
       }
+    },
+    components: {
+      KalixDialog: Dialog
     }
   }
 </script>
 
 <style scoped>
-
+  .transfer-footer {
+    margin-left: 20px;
+    padding: 6px 5px;
+  }
 </style>
