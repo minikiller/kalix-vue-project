a260b714a8ab24c7c3e90f74432068277d7e082b
fengjing <739722894@qq.com>
2017-10-30 16:49:40 +0800

    修改数据权限选取用户组建

--- src/views/admin/dataauth/dataauthAddUser.vue
+++ src/views/admin/dataauth/dataauthAddUser.vue
@@ -1,41 +1,62 @@
 <template lang="pug">
   kalix-dialog.dataauth-addUser(bizKey="dataauth" ref="kalixBizDialog"
-  style="width:1100px;left:20%")
-   div.el-dialog__header(style="#5fa2dd")
-   div.el-form(slot="dialogFormSlot" )
-    el-transfer(v-model="value3" style="width:100%" filterable
-    v-bind:left-default-checked="[2, 3]"
-    v-bind:right-default-checked="[1]"
-    v-bind:titles="['可选项', '已选项']"
-    v-bind:footer-format="{noChecked: '${total}', hasChecked: '${checked}/${total}'}"
-    v-bind:data="data2")
+  style="width:1100px;left:20%" v-bind:form-model.sync="formModel" v-on:onSubmitClick="onSubmitClick")
+    div.el-form(slot="dialogFormSlot" )
+      el-transfer(v-model="userList" style="width:100%" filterable
+      v-bind:left-default-checked="[2, 3]"
+      v-bind:right-default-checked="[1]"
+      v-bind:titles="['可选项', '已选项']"
+      v-bind:footer-format="{noChecked: '${total}', hasChecked: '${checked}/${total}'}"
+      v-bind:data="dataList"
+      v-on:change="handleChange")
 </template>
 
 <script>
   import Dialog from '@/components/custom/baseDialog.vue'
+  import FormModel from './model'
+  import {usersURL} from 'views/admin/config.toml'
   export default {
     data() {
-      const generateData = _ => {
-        console('11111222222233333333')
-        const DictKey = '$USER-KEY'
-        console('DictKey', DictKey)
-        const data = []
-        for (let i = 1; i <= 15; i++) {
-          data.push({
-            key: i,
-            label: '备选项',
-            disabled: i % 4 === 0
+      return {
+        userList: [],
+        formModel: Object.assign({}, FormModel),
+        dataList: [],
+        ids: []
+      }
+    },
+    created() {
+      // 如果有传入 defaultIds
+      this.axios.get(usersURL, {}).then(response => {
+        this.userList = response.data.data
+        for (let i = 0; i < this.userList.length; i++) {
+          this.dataList.push({
+            key: this.userList[i].id,
+            label: this.userList[i].loginName
           })
         }
-        return data
-      }
-      return {
-        data2: generateData(),
-        value3: [1]
-      }
+      })
+    },
+    mounted() {
     },
     components: {
       KalixDialog: Dialog
+    },
+    methods: {
+      handleChange(value, direction, movedKeys) {
+        const ids = []
+        let userIds = ''
+        for (let i = 0; i < movedKeys.length - 1; i++) {
+          userIds = userIds + movedKeys[i] + ','
+        }
+        userIds = userIds + movedKeys[movedKeys.length - 1]
+        ids[0] = this.formModel.id
+        ids[1] = userIds
+        this.ids = ids
+      },
+      onSubmitClick() {
+        this.axios.post('/dataauths/{this.formModel.id}/users', this.ids)
+      }
+
     }
   }
 </script>
