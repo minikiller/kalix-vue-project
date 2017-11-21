799a3b7ce1e77406766ff1ee0a0a1ef2c2e17d6b
minikiller@qq.com <minikiller@qq.com>
2017-11-21 16:01:09 +0800

    增加组织结构树组件

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,10 @@ module.exports = {
     'generator-star-spacing': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    'space-before-function-paren': 0
+    'space-before-function-paren': 0,
+    'one-var':0,
+    "no-extend-native": 0,
+    "operator-linebreak": [2, "after"],
+    "indent": [2, 2, { "SwitchCase": 1 }]
   }
 }
