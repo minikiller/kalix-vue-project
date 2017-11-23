efbdcfb17ad9a39f53930809f5fda491d3d14bfc
minikiller@qq.com <minikiller@qq.com>
2017-11-23 16:57:42 +0800

    增加带logo的qrcode二维码组件

---
+++ src/views/art/student/student.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  div
+    kalix-qrcode(:value="'http://baidu.com'")
+</template>
+
+<script type="text/ecmascript-6">
+  import KalixQrcode from '@/components/qrcode/qrcode'
+
+  export default {
+    components: {
+      KalixQrcode
+    }
+  }
+</script>
+
+<style scoped lang="stylus">
+
+</style>
