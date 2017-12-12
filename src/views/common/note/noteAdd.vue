3495214ff92fb3683ee11b4c87a2535e53b9c823
minikiller@qq.com <minikiller@qq.com>
2017-12-12 14:46:29 +0800

    fixed note error

--- src/views/common/note/noteAdd.vue
+++ src/views/common/note/noteAdd.vue
@@ -6,7 +6,7 @@
 
 <template lang="pug">
   kalix-dialog.user-add(bizKey="note"
-  ref="kalixBizDialog" v-bind:form-model="formModel" v-bind:targetURL="targetURL"
+  ref="kalixBizDialog" v-bind:form-model.sync="formModel" v-bind:targetURL="targetURL"
   )
     div.el-form(slot="dialogFormSlot")
       el-form-item(label="标题" prop="title" v-bind:rules="rules.title")
@@ -18,20 +18,12 @@
 <script type="text/ecmascript-6">
   import Dialog from '@/components/custom/baseDialog.vue'
   import {NoteURL} from '../config.toml'
+  import {FormModel} from './model.toml'
 
   export default {
-    props: {
-      formModel: {
-        type: Object,
-        required: true
-      },
-      formRules: {
-        type: Object,
-        required: true
-      }
-    },
     data() {
       return {
+        formModel: Object.assign({}, FormModel),
         rules: {
           title: [{required: true, message: '请输入标题', trigger: 'blur'}],
           content: [{required: true, message: '请输入内容', trigger: 'blur'}]
