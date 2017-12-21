4d3720613d3632e06dc2ccddd97e9a2b0bdf0167
minikiller@qq.com <minikiller@qq.com>
2017-12-21 16:12:25 +0800

    红头文件申请流程启动成功。

--- src/element-ui.js
+++ src/element-ui.js
@@ -55,8 +55,8 @@ import {
   // Step,
   // Carousel,
   // CarouselItem,
-  // Collapse,
-  // CollapseItem,
+  Collapse,
+  CollapseItem,
   Cascader,
   // ColorPicker,
   Transfer,
@@ -128,8 +128,8 @@ Vue.use(Card)
 // Vue.use(Step)
 // Vue.use(Carousel)
 // Vue.use(CarouselItem)
-// Vue.use(Collapse)
-// Vue.use(CollapseItem)
+Vue.use(Collapse)
+Vue.use(CollapseItem)
 Vue.use(Cascader)
 // Vue.use(ColorPicker)
 Vue.use(Container)
--- src/views/oa/redheadapply/RedheadApplyModifyForm.vue
+++ src/views/oa/redheadapply/RedheadApplyModifyForm.vue
@@ -6,30 +6,30 @@
 <template lang="pug">
   div.el-form.kalix-form-table(slot="dialogFormSlot")
     div.table-title 吉林动画学院红头文件申请表
-    el-form-item(label="名称" v-bind:label-width="labelWidth")
+    el-form-item(label="名称" v-bind:label-width="labelWidth" prop="title" v-bind:rules="rules.title")
       el-input(v-model="formModel.title")
+    el-form-item(label="文件种类" v-bind:label-width="labelWidth" prop="docCategory" v-bind:rules="rules.docCategory")
+      el-radio-group(v-model="formModel.docCategory")
+        el-radio(label="对内") 对内
+        el-radio(label="对外") 对外
     div.s-flex
-      el-form-item.s-flex_item.kalix-form-table-td(label="申请部门" v-bind:label-width="labelWidth")
+      el-form-item.s-flex_item.kalix-form-table-td(label="申请部门" v-bind:label-width="labelWidth" prop="orgId" v-bind:rules="rules.orgId")
         kalix-org-select(v-model="formModel.orgId" v-on:selectChange="onOrgIdChange")
-      el-form-item.s-flex_item.kalix-form-table-td(label="用印数" v-bind:label-width="labelWidth")
-        el-input-number(v-model="formModel.usageCount" v-bind:min="1" v-bind:max="10" style="width:100%")
-    el-form-item(label="发文类别" v-bind:label-width="labelWidth")
-      kalix-dict-select(v-model="formModel.sealType" appName="oa" dictType="发文类型")
-    el-form-item(label="备注" v-bind:label-width="labelWidth")
-      el-input(v-model="formModel.remark")
+      el-form-item.s-flex_item(label="文号类型" v-bind:label-width="labelWidth"  prop="docType" v-bind:rules="rules.docType")
+        kalix-dict-select(v-model="formModel.docType" appName="oa" dictType="文号类型")
+    el-form-item.s-flex_item.kalix-form-table-td(label="发文内容" v-bind:label-width="labelWidth" prop="docContent" v-bind:rules="rules.docContent")
+      el-input(v-model="formModel.docContent" type="textarea")
     div.s-flex
-      el-form-item.s-flex_item.kalix-form-table-td(label="部门负责人" v-bind:label-width="labelWidth")
-        el-input(v-model="formModel.departmentHead" readonly)
-      el-form-item.s-flex_item.kalix-form-table-td(label="分公司负责人" v-bind:label-width="labelWidth")
-        el-input(v-model="formModel.tableFormField" readonly)
+      el-form-item.s-flex_item.kalix-form-table-td(label="部门负责人签字" v-bind:label-width="labelWidth")
+        el-input(v-model="formModel.depUser" readonly)
+      el-form-item.s-flex_item.kalix-form-table-td(label="校务部签字" v-bind:label-width="labelWidth")
+        el-input(v-model="formModel.schoolUser" readonly)
     div.s-flex
-      el-form-item.s-flex_item.kalix-form-table-td(label="法律顾问" v-bind:label-width="labelWidth")
-        el-input(v-model="formModel.counsel"  readonly)
-      el-form-item.s-flex_item.kalix-form-table-td(label="总经理" v-bind:label-width="labelWidth")
-        el-input(v-model="formModel.generalManager" readonly)
+      el-form-item.s-flex_item.kalix-form-table-td(label="校领导签字" v-bind:label-width="labelWidth")
+        el-input(v-model="formModel.managerUser"  readonly)
+      el-form-item.s-flex_item.kalix-form-table-td(label="董事长签字" v-bind:label-width="labelWidth")
+        el-input(v-model="formModel.chairmanUser" readonly)
     div.s-flex
-      el-form-item.s-flex_item.kalix-form-table-td(label="发文专管员" v-bind:label-width="labelWidth")
-        el-input(v-model="formModel.sealAdministrator" readonly)
       el-form-item.s-flex_item.kalix-form-table-td(label="经办人" v-bind:label-width="labelWidth")
         el-input(v-model="formModel.createBy" readonly)
 </template>
