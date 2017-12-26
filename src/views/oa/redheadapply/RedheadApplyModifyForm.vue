b3b8d2d192cc2edfa408b95fde292f2a1d5caafe
minikiller@qq.com <minikiller@qq.com>
2017-12-26 09:18:49 +0800

    完成级联控件的使用

--- src/views/oa/redheadapply/RedheadApplyModifyForm.vue
+++ src/views/oa/redheadapply/RedheadApplyModifyForm.vue
@@ -8,15 +8,11 @@
     div.table-title 吉林动画学院红头文件申请表
     el-form-item(label="名称" v-bind:label-width="labelWidth" prop="title" v-bind:rules="rules.title")
       el-input(v-model="formModel.title")
-    el-form-item(label="文件种类" v-bind:label-width="labelWidth" prop="docCategory" v-bind:rules="rules.docCategory")
-      el-radio-group(v-model="formModel.docCategory")
-        el-radio(label="对内") 对内
-        el-radio(label="对外") 对外
     div.s-flex
       el-form-item.s-flex_item.kalix-form-table-td(label="申请部门" v-bind:label-width="labelWidth" prop="orgId" v-bind:rules="rules.orgId")
         kalix-org-select(v-model="formModel.orgId" v-on:selectChange="onOrgIdChange")
-      el-form-item.s-flex_item(label="文号类型" v-bind:label-width="labelWidth"  prop="docType" v-bind:rules="rules.docType")
-        kalix-dict-select(v-model="formModel.docType" appName="oa" dictType="文号类型")
+      el-form-item.s-flex_item(label="文号类型" v-bind:label-width="labelWidth"  prop="docAssort" v-bind:rules="rules.docAssort")
+        el-cascader(expand-trigger="hover"    v-bind:options="options"    v-model="formModel.docAssort" v-bind:show-all-levels="false"  v-on:change="onChange")
     el-form-item.s-flex_item.kalix-form-table-td(label="发文内容" v-bind:label-width="labelWidth" prop="docContent" v-bind:rules="rules.docContent")
       el-input(v-model="formModel.docContent" type="textarea")
     div.s-flex
@@ -41,9 +37,15 @@
   import BaseDictSelect from '@/components/custom/baseDictSelect'
 
   export default {
-    props: ['formModel'],
+    props: {
+      formModel: {
+        type: Object,
+        default: {}
+      }
+    },
     created() {
       this.labelWidth = '110px'
+      this.init(this.formModel.docType)
     },
     data() {
       return {
@@ -51,15 +53,83 @@
         rules: {
           title: [{required: true, message: '请输入名称', trigger: 'blur'}],
           orgId: [{type: 'number', required: true, message: '请选择申请部门', trigger: 'change'}],
-          docType: [{type: 'number', required: true, message: '请选择文号类型', trigger: 'change'}],
-          docCategory: [{required: true, message: '请选择文件种类', trigger: 'change'}],
+          docAssort: [{required: true, message: '请选择文号类型', trigger: 'change'}],
           docContent: [{required: true, message: '请输入发文内容', trigger: 'blur'}]
-        }
+        },
+        options: [{
+          value: '对外',
+          label: '对外',
+          children: [{
+            label: '吉动院字',
+            value: '1'
+          }]
+        }, {
+          value: '对内',
+          label: '对内',
+          children: [
+            {
+              label: '吉动院人字',
+              value: '2'
+            },
+            {
+              label: '吉动院教字',
+              value: '3'
+            },
+            {
+              label: '吉动院学字',
+              value: '4'
+            },
+            {
+              label: '吉动院招字',
+              value: '5'
+            },
+            {
+              label: '吉动院研字',
+              value: '6'
+            },
+            {
+              label: '吉动院学位字',
+              value: '7'
+            },
+            {
+              label: '吉动校友字',
+              value: '8'
+            },
+            {
+              label: '吉动院研学字',
+              value: '9'
+            },
+            {
+              label: '吉动院字',
+              value: '10'
+            }
+          ]
+        }]
       }
     },
     methods: {
       onOrgIdChange(item) {
         this.formModel.orgName = item.name
+      },
+      onChange(value) {
+        this.formModel.docType = value[1]
+      },
+      init(val) {
+        if (val > 1) {
+          this.formModel.docAssort = ['对内', String(val)]
+        } else {
+          this.formModel.docAssort = ['对外', '1']
+        }
+      }
+    },
+    watch: {
+      'formModel': {
+        handler: function (val, oldVal) {
+        },
+        deep: true
+      },
+      'formModel.docType': function (val, oldVal) {
+        this.init(val)
       }
     },
     components: {
