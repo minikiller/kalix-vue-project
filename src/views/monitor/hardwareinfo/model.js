eec24a344ccbc8ce637ee91e2d55262d7a8f7d98
fengweibin <tx_xinxin2006@163.com>
2017-12-29 10:09:32 +0800

    硬件管理

---
+++ src/views/monitor/hardwareinfo/model.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Administrator on 2017/9/22.
+ */
+const FormModel = {
+  userId: null,
+  name: '',
+  studentNo: '',
+  classId: null,
+  className: '',
+  instructor: '',
+  identificationCard: '',
+  sex: '',
+  birthday: '',
+  nation: '',
+  placeOfOrigin: '',
+  politicalStatus: '',
+  joinPartyDate: '',
+  address: '',
+  postalcode: '',
+  homePhone: '',
+  province: '',
+  entranceYear: '',
+  trainingLevel: '',
+  period: ''
+}
+export default FormModel
