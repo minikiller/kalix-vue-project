7c1c9591e133a676aa35dd66b52df732d58d7ca8
fengjing <739722894@qq.com>
2017-12-29 16:17:15 +0800

    新增角色管理功能

--- src/views/admin/role/role.vue
+++ src/views/admin/role/role.vue
@@ -1,18 +1,15 @@
 <template lang="pug">
   keep-alive
     base-table(bizKey="role" title='角色列表' v-bind:targetURL="targetURL"
-    v-bind:bizDialog="bizDialog" v-bind:tableFields="tableFields" bizSearch="AdminUserSearch" v-bind:btnList="btnList"
+    v-bind:bizDialog="bizDialog" v-bind:tableFields="tableFields" bizSearch="AdminRoleSearch" v-bind:btnList="btnList"
     v-bind:customTableTool="customTableTool" v-bind:buttonPermissionPrefix="buttonPermissionPrefix"
     v-bind:dictDefine="dictDefine" ref="userTable")
 </template>
 <script type="text/ecmascript-6">
   import BaseTable from '@/components/custom/baseTable'
-  import {rolesURL, userBtnPermissionPrefix, RoleComponent} from '../config.toml'
-  import {userBtnList} from '../user/index'
+  import {rolesURL, roleBtnPermissionPrefix, RoleComponent} from '../config.toml'
+  import {roleBtnList} from '../role/index'
   import {registerComponent} from '@/api/register'
-  import Message from 'common/message'
-  import {ON_REFRESH_DATA} from '@/components/custom/event.toml'
-  import EventBus from 'common/eventbus'
 
   // 注册全局组件
   registerComponent(RoleComponent)
@@ -31,25 +28,22 @@
           targetField: 'positionName',
           sourceField: 'position'
         }],
-        btnList: userBtnList,
-        buttonPermissionPrefix: userBtnPermissionPrefix,
+        btnList: roleBtnList,
+        buttonPermissionPrefix: roleBtnPermissionPrefix,
         targetURL: rolesURL,
         tableFields: [
-          {prop: 'userTypeName', label: '用户类型'},
-          {prop: 'code', label: '用户代码'},
-          {prop: 'loginName', label: '登录名'},
-          {prop: 'name', label: '名称'},
-          {prop: 'sex', label: '性别'},
-          {prop: 'email', label: '邮箱'},
-          {prop: 'phone', label: '固定电话'},
-          {prop: 'mobile', label: '手机'}
+          {prop: 'app', label: '所属应用'},
+          {prop: 'name', label: '角色名称'},
+          {prop: 'remark', label: '备注'},
+          {prop: 'createBy', label: '创建人'},
+          {prop: 'creationDate', label: '创建时间'}
         ],
         bizDialog: [
-          {id: 'view', dialog: 'AdminUserView'},
-          {id: 'edit', dialog: 'AdminUserEdit'},
-          {id: 'add', dialog: 'AdminUserAdd'},
-          {id: 'key', dialog: 'AdminUserResetpwd'},
-          {id: 'auth', dialog: 'AdminUserAuthView'}
+          {id: 'view', dialog: 'AdminRoleView'},
+          {id: 'edit', dialog: 'AdminRoleEdit'},
+          {id: 'add', dialog: 'AdminRoleAdd'},
+          {id: 'addUser', dialog: 'AdminRoleAddUser'},
+          {id: 'auth', dialog: 'AdminRoleAuth'}
         ]
       }
     },
@@ -61,41 +55,6 @@
     methods: {
       customTableTool(row, btnId, that) {
         switch (btnId) {
-          case 'startStopUsing': { // 启用/停用
-            let warnInfo = '确定要' + (row.available === 1 ? '停用' : '启用') + '该用户吗？'
-            that.$confirm(warnInfo, '警告', {
-              confirmButtonText: '确定',
-              cancelButtonText: '取消',
-              type: 'warning'
-            }).then(() => {
-              return this.axios.request({
-                method: 'PUT',
-                url: this.targetURL + '/' + row.id,
-                params: {},
-                data: {
-                  id: row.id,
-                  available: (row.available === 1 ? 0 : 1)
-                }
-              })
-            }).then((res) => {
-              console.log(res)
-              Message.processResult(res)
-              EventBus.$emit(ON_REFRESH_DATA)
-            })
-            break
-          }
-          case 'key': { // 重置密码
-            that.whichBizDialog = ''
-            let dig =
-              that.bizDialog.filter((item) => {
-                return item.id === 'key'
-              })
-            that.whichBizDialog = dig[0].dialog
-            setTimeout(() => {
-              that.$refs.kalixDialog.$refs.kalixBizDialog.open('', true, row)
-            }, 20)
-            break
-          }
           case 'auth' : { // 权限查看
             that.whichBizDialog = ''
             let dig =
@@ -108,6 +67,16 @@
             }, 20)
             break
           }
+          case 'addUser':
+            that.whichBizDialog = ''
+            let dig = that.bizDialog.filter((item) => {
+              return item.id === 'addUser'
+            })
+            that.whichBizDialog = dig[0].dialog
+            setTimeout(() => {
+              that.$refs.kalixDialog.open('添加用户', row)
+            }, 20)
+            break
         }
       }
     }
