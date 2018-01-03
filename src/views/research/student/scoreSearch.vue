73b5c1a4f676f71b8a456aded33ec24b66393044
houqingjie <150091225@qq.com>
2018-01-03 10:57:35 +0800

    修改科研管理学生信息功能，并测试学生成绩功能（主子表使用，主表学生信息，子表学生成绩）

---
+++ src/views/research/student/scoreSearch.vue
@@ -0,0 +1,29 @@
+<!--
+描述：科研管理-人员管理-学生信息-学生成绩-查询组件
+开发人：hqj
+开发日期：2017年12月20日
+-->
+
+<template lang="pug">
+  kalix-search(title="学生成绩查询" v-bind:searchFields="searchFields")
+</template>
+
+<script type="text/ecmascript-6">
+  import BaseSearch from '@/components/custom/baseSearch.vue'
+
+  export default {
+    data() {
+      return {
+        searchFields: [
+          {label: '学号', prop: 'code'}
+        ]
+      }
+    },
+    components: {
+      KalixSearch: BaseSearch
+    }
+  }
+</script>
+
+<style scoped lang="stylus" type="text/stylus">
+</style>
--- src/views/research/student/student.vue
+++ src/views/research/student/student.vue
@@ -1,19 +1,29 @@
 <!--
 描述：科研管理-人员管理-学生信息
 开发人：hqj-修改
-开发日期：2017年12月18日
+开发日期：2018年01月02日
 -->
 <template lang="pug">
   keep-alive
-    base-table(bizKey="researchStudent" title='学生信息列表' v-bind:targetURL="targetURL"
-    v-bind:bizDialog="bizDialog" v-bind:tableFields="tableFields" bizSearch="ResearchStudentSearch"
-    v-bind:btnList="btnList" v-bind:toolbarBtnList="toolbarBtnList" v-bind:customTableTool="customTableTool")
+    div(style="position: absolute;width: 100%;height: 100%;overflow: auto;")
+      div(style="height:500px;position:relative")
+        base-table(bizKey="researchStudent" title='学生信息列表' v-bind:targetURL="targetURL"
+        v-bind:bizDialog="bizDialog" v-bind:tableFields="tableFields" bizSearch="ResearchStudentSearch"
+        v-bind:btnList="btnList" v-bind:toolbarBtnList="toolbarBtnList" v-bind:customTableTool="customTableTool"
+        v-bind:dictDefine="dictDefine" v-on:onTableRowClick="onTableRowClick")
+      div(style="height:500px;position:relative")
+        base-table(bizKey="researchScore" title='学生成绩列表' v-bind:targetURL="detailTargetURL"
+        v-bind:bizDialog="detailBizDialog" v-bind:tableFields="detailTableFields" v-bind:btnList="detailBtnList"
+        v-bind:toolbarBtnList="detailToolbarBtnList" v-bind:customToolBar="detailCustomToolBar" v-bind:customTableTool="detailCustomTableTool"
+        v-bind:jsonStr="detailJsonStr" ref="scoreTable")
 </template>
 
 <script type="text/ecmascript-6">
+  import DetailFormModel from './scoreModel'
   import BaseTable from '@/components/custom/baseTable'
-  import {StudentURL, ResearchStudentComponent, ToolButtonList1} from '../config.toml'
+  import {StudentURL, ResearchStudentComponent, ToolButtonPersonList, ToolButtonScoreList} from '../config.toml'
   import {registerComponent} from '@/api/register'
+  import Message from 'common/message'
 
   // 注册全局组件
   registerComponent(ResearchStudentComponent)
@@ -21,7 +31,13 @@
   export default {
     data() {
       return {
-        btnList: ToolButtonList1,
+        dictDefine: [{ // 定义数据字典的显示
+          cacheKey: 'RESEARCH-DICT-KEY',
+          type: '省份',
+          targetField: 'provinceName',
+          sourceField: 'province'
+        }],
+        btnList: ToolButtonPersonList,
         toolbarBtnList: [
           {id: 'add', isShow: false}
         ],
@@ -31,7 +47,7 @@
           {prop: 'name', label: '姓名'},
           {prop: 'sex', label: '性别'},
           {prop: 'majorName', label: '专业'},
-          {prop: 'province', label: '生源省份'},
+          {prop: 'provinceName', label: '生源省份'},
           {prop: 'entranceYear', label: '入学年份'},
           {prop: 'phone', label: '固定电话'},
           {prop: 'mobile', label: '手机'},
@@ -41,7 +57,24 @@
         bizDialog: [
           {id: 'view', dialog: 'ResearchStudentView'},
           {id: 'newEdit', dialog: 'ResearchStudentAdd'}
-        ]
+        ],
+        detailBtnList: ToolButtonScoreList,
+        detailToolbarBtnList: [
+          {id: 'add', isShow: false},
+          {id: 'newAdd', title: '添加', isShow: true, icon: 'icon-add'}
+        ],
+        detailTargetURL: '',
+        detailTableFields: [
+          {prop: 'code', label: '学号'},
+          {prop: 'name', label: '姓名'},
+          {prop: 'majorCourseName', label: '专业课程'},
+          {prop: 'score', label: '成绩'}
+        ],
+        detailBizDialog: [
+          {id: 'newEdit', dialog: 'ResearchScoreAdd'},
+          {id: 'newAdd', dialog: 'ResearchScoreAdd'}
+        ],
+        detailJsonStr: ''
       }
     },
     components: {
@@ -53,22 +86,98 @@
       customTableTool(row, btnId, that) {
         switch (btnId) {
           case 'newEdit' : { // 新编辑
-            that.whichBizDialog = ''
-            let dig =
-              that.bizDialog.filter((item) => {
-                return item.id === 'newEdit'
-              })
-            that.whichBizDialog = dig[0].dialog
-            setTimeout(() => {
-              if (row.id && row.id > 0) { // 修改
-                that.$refs.kalixDialog.$refs.kalixBizDialog.open('', true, row)
-              } else { // 新增
-                this.$delete(row, 'version')
-                this.$delete(row, 'version_')
-                that.$refs.kalixDialog.$refs.kalixBizDialog.open('', false, row)
-              }
-            }, 20)
+            this.onNewEdit()
             break
+          }
+        }
+      },
+      onNewEdit(row, table) {
+        table.whichBizDialog = ''
+        let dig =
+          table.bizDialog.filter((item) => {
+            return item.id === 'newEdit'
+          })
+        table.whichBizDialog = dig[0].dialog
+        setTimeout(() => {
+          if (row.id && row.id > 0) { // 修改
+            table.$refs.kalixDialog.$refs.kalixBizDialog.open('编辑', true, row)
+          } else { // 新增
+            this.$delete(row, 'version')
+            this.$delete(row, 'version_')
+            table.$refs.kalixDialog.$refs.kalixBizDialog.open('编辑', false, row)
+          }
+        }, 20)
+      },
+      detailCustomToolBar(btnId, table) {
+        switch (btnId) {
+          case 'newAdd': {
+            this.onDetailNewAdd(table)
+            break
+          }
+        }
+      },
+      onDetailNewAdd(table) {
+        let dig =
+          table.bizDialog.filter((item) => {
+            return item.id === 'newAdd'
+          })
+        table.whichBizDialog = dig[0].dialog
+        setTimeout(() => {
+          this.detailFormModel = Object.assign({}, DetailFormModel)
+          if (this.detailRow && this.detailRow.id) {
+            this.detailFormModel.studentId = this.detailRow.id
+            this.detailFormModel.code = this.detailRow.code
+            this.detailFormModel.name = this.detailRow.name
+            this.detailFormModel.majorId = this.detailRow.majorId
+            this.detailFormModel.majorName = this.detailRow.majorName
+            this.detailFormModel.targetURL = this.detailTargetURL
+            table.$refs.kalixDialog.$refs.kalixBizDialog.open('添加', false, this.detailFormModel)
+          } else {
+            Message.error('学生信息无效,无法添加成绩! 请先选择学生并确认已经维护过其详细信息!')
+          }
+        }, 20)
+      },
+      detailCustomTableTool(row, btnId, that) {
+        switch (btnId) {
+          case 'newEdit' : {
+            this.onDetailNewEdit(row, that)
+            break
+          }
+        }
+      },
+      onDetailNewEdit(row, table) {
+        let dig =
+          table.bizDialog.filter((item) => {
+            return item.id === 'newEdit'
+          })
+        table.whichBizDialog = dig[0].dialog
+        setTimeout(() => {
+          // this.detailFormModel = Object.assign({}, row)
+          if (this.detailRow) {
+//            this.detailFormModel.studentId = this.detailRow.id
+//            this.detailFormModel.code = this.detailRow.code
+//            this.detailFormModel.name = this.detailRow.name
+//            this.detailFormModel.majorId = this.detailRow.majorId
+//            this.detailFormModel.majorName = this.detailRow.majorName
+//            this.detailFormModel.majorCourseId = row.majorCourseId
+//            this.detailFormModel.score = row.score
+//            this.detailFormModel.targetURL = this.detailTargetURL
+//            table.$refs.kalixDialog.$refs.kalixBizDialog.open('编辑', true, this.detailFormModel)
+            row.targetURL = this.detailTargetURL
+            table.$refs.kalixDialog.$refs.kalixBizDialog.open('编辑', true, row)
+          } else {
+            Message.error('学生信息无效,无法编辑成绩! 请先选择学生并确认已经维护过其详细信息!')
+          }
+        }, 20)
+      },
+      onTableRowClick(row, event, column) {
+        this.$refs.scoreTable.clearData()
+        this.detailRow = row
+        this.id = row.id
+        if (this.id) {
+          this.detailTargetURL = StudentURL + '/' + this.id + '/scores'
+          if (this.$refs.scoreTable) {
+            this.$refs.scoreTable.getData()
           }
         }
       }
@@ -77,5 +186,4 @@
 </script>
 
 <style scoped lang="stylus">
-
 </style>
