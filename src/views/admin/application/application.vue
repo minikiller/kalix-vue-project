964894bc1bd04cbb48dd8e4443319d1591106a30
yangze8818 <yangze_lmkj@163.com>
2018-01-08 13:37:17 +0800

    完成系统应用-应用管理

---
+++ src/views/admin/application/application.vue
@@ -0,0 +1,80 @@
+<template lang="pug">
+  keep-alive
+    base-table(bizKey="application" title="应用列表" v-bind:targetURL="targetURL"
+    v-bind:bizDialog="bizDialog" v-bind:tableFields="tableFields" bizSearch="AdminApplicationSearch"
+    v-bind:buttonPermissionPrefix="buttonPermissionPrefix" v-bind:btnList="btnList" v-bind:toolbarBtnList="toolbarBtnList"
+    v-bind:customTableTool="customTableTool" ref="applicationTable" )
+</template>
+
+<script type="text/ecmascript-6">
+  import BaseTable from '@/components/custom/baseTable'
+  import {appsURL, appsStartURL, appsStopURL, applicationBtnPermissionPrefix, ApplicationComponent} from '../config.toml'
+  import {applicationBtnList} from '../application/index'
+  import {registerComponent} from '@/api/register'
+  // import Message from 'common/message'
+  import {ON_REFRESH_DATA} from '@/components/custom/event.toml'
+  import EventBus from 'common/eventbus'
+
+  registerComponent(ApplicationComponent)
+
+  export default {
+    components: {
+      BaseTable
+    },
+    data() {
+      return {
+        btnList: applicationBtnList,
+        buttonPermissionPrefix: applicationBtnPermissionPrefix,
+        targetURL: appsURL,
+        startURL: appsStartURL,
+        stopURL: appsStopURL,
+        toolbarBtnList: [
+          {id: 'add', isShow: false}
+        ],
+        tableFields: [
+          {prop: 'name', label: '名称'},
+          {prop: 'code', label: '应用代码'},
+          {prop: 'iconCls', label: '应用图标'}
+        ],
+        bizDialog: [
+          {id: 'view', dialog: 'AdminApplicationView'}
+        ]
+      }
+    },
+    methods: {
+      customTableTool(row, btnId, that) {
+        switch (btnId) {
+          case 'startStopUsing': { // 启用/停用
+            let warnInfo = '确定要' + (row.active === true ? '停用' : '启用') + '该应用吗？'
+            that.$confirm(warnInfo, '警告', {
+              confirmButtonText: '确定',
+              cancelButtonText: '取消',
+              type: 'warning'
+            }).then(() => {
+              return this.axios.request({
+                method: 'GET',
+                url: (row.active === true ? this.stopURL : this.startURL),
+                params: {
+                  id: row.code
+                },
+                data: {
+                  id: row.code,
+                  available: row.active
+                }
+              })
+            }).then((res) => {
+              console.log(res)
+              // Message.processResult(res)
+              EventBus.$emit(ON_REFRESH_DATA)
+            })
+            break
+          }
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus" type="text/stylus">
+
+</style>
---
+++ src/views/admin/application/applicationView.vue
@@ -0,0 +1,33 @@
+<!--
+描述：系统应用-应用管理-查看组件
+开发人：yangz
+开发日期：2018年01月14日
+-->
+<template lang="pug">
+  kalix-dialog.user-add(title='查看' bizKey="application" ref="kalixBizDialog" v-bind:formModel.sync="formModel" isView)
+    div.el-form(slot="dialogFormSlot")
+      el-form-item(label="名称" prop="name" v-bind:label-width="labelWidth")
+        el-input(v-model="formModel.name" readonly)
+      el-form-item(label="应用代码" prop="code" v-bind:label-width="labelWidth")
+        el-input(v-model="formModel.code" readonly)
+      el-form-item(label="应用图标" prop="iconCls" v-bind:label-width="labelWidth")
+        el-input(v-model="formModel.iconCls" readonly)
+      el-form-item(label="备注" prop="remark" v-bind:label-width="labelWidth")
+        el-input(type="textarea" v-model="formModel.remark" v-bind:rows="3" readonly)
+</template>
+
+<script type="text/ecmascript-6">
+  import FormModel from './model'
+  import Dialog from '@/components/custom/baseDialog.vue'
+  export default {
+    data() {
+      return {
+        formModel: Object.assign({}, FormModel),
+        labelWidth: '140px'
+      }
+    },
+    components: {
+      KalixDialog: Dialog
+    }
+  }
+</script>
