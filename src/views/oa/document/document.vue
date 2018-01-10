95d2d217ccd2644aa71965a1a1e29b84c0dc438c
houqingjie <150091225@qq.com>
2018-01-10 14:18:31 +0800

    修改文号管理表格显示样式

--- src/views/oa/document/document.vue
+++ src/views/oa/document/document.vue
@@ -6,15 +6,37 @@
 
 <template lang="pug">
   keep-alive
-    base-table(bizKey="document" title='文号列表' v-bind:tableFields="tableFields" v-bind:targetURL="targetURL"
+    base-table(bizKey="document" title='文号列表' v-bind:targetURL="targetURL"
     v-bind:bizDialog="bizDialog" bizSearch="OaDocumentSearch" v-bind:btnList="btnList"
-    v-bind:toolbarBtnList="toolbarBtnList" v-bind:customTableTool="customTableTool")
+    v-bind:toolbarBtnList="toolbarBtnList" v-bind:customTableTool="customTableTool"
+    v-bind:isFixedColumn="isFixedColumn" v-bind:dictDefine="dictDefine")
+      template(slot="tableColumnSlot")
+        kalix-biz-no-column(title="文号")  // 业务编号
+        el-table-column(prop="docTypeName" label="文号类型" align="center" width="120")
+        el-table-column(prop="year" label="年份" align="center" width="100")
+        el-table-column(prop="status" label="文号状态" align="center" width="100")
+          template(slot-scope="scope")
+            template(v-if="scope.row.status === '使用中'")
+              el-tag(type="success") {{ scope.row.status }}
+            template(v-else-if="scope.row.status === '已撤回'")
+              el-tag(type="warning") {{ scope.row.status }}
+            template(v-else-if="scope.row.status === '已废除'")
+              el-tag(type="danger") {{ scope.row.status }}
+            template(v-else)
+              el-tag {{ scope.row.status }}
+        el-table-column(prop="docDate" label="发文时间" align="center" width="220")
+        el-table-column(prop="docDept" label="发文部门" align="center" width="220")
+        el-table-column(prop="title" label="文题" align="center" width="300")
+        kalix-date-column(prop="creationDate" label="创建时间")
+        kalix-date-column(prop="updateDate" label="更新时间")
 </template>
 
 <script type="text/ecmascript-6">
   import BaseTable from '@/components/custom/baseTable'
   import {DocumentURL, DocumentComponent, DocumentToolButtonList} from '../config.toml'
   import {registerComponent} from '@/api/register'
+  import BizNoColumn from '@/views/oa/comp/bizNoColumn'
+  import DateColumn from 'views/oa/comp/dateColumn'
 
   // 注册全局组件
   registerComponent(DocumentComponent)
@@ -28,30 +50,32 @@
           targetField: 'docTypeName',
           sourceField: 'docType'
         }],
+        isFixedColumn: true,
         btnList: DocumentToolButtonList,
         toolbarBtnList: [
           {id: 'add', isShow: false}
         ],
         targetURL: DocumentURL,
-        tableFields: [
-          {prop: 'docTypeName', label: '文号类型'},
-          {prop: 'year', label: '年份', width: '100'},
-          {prop: 'number', label: '编号'},
-          {prop: 'businessNo', label: '文号', width: '200'},
-          {prop: 'status', label: '文号状态', width: '100'},
-          {prop: 'docDate', label: '发文时间'},
-          {prop: 'docDept', label: '发文部门'},
-          {prop: 'title', label: '文题', width: '140'},
-          {prop: 'creationDate', label: '创建时间', width: '160'},
-          {prop: 'updateDate', label: '更新时间', width: '160'}
-        ],
+//        tableFields: [
+//          {prop: 'docTypeName', label: '文号类型', width: '120'},
+//          {prop: 'year', label: '年份', width: '100'},
+//          {prop: 'businessNo', label: '文号', width: '200'},
+//          {prop: 'status', label: '文号状态', width: '100'},
+//          {prop: 'docDate', label: '发文时间', width: '160'},
+//          {prop: 'docDept', label: '发文部门', width: '200'},
+//          {prop: 'title', label: '文题', width: '280'},
+//          {prop: 'creationDate', label: '创建时间', width: '160'},
+//          {prop: 'updateDate', label: '更新时间', width: '160'}
+//        ],
         bizDialog: [
           {id: 'view', dialog: 'OaDocumentView'}
         ]
       }
     },
     components: {
-      BaseTable
+      BaseTable,
+      KalixBizNoColumn: BizNoColumn,
+      KalixDateColumn: DateColumn
     },
     created() {
     },
