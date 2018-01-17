835ad3bd1aa685cc15c47b2bf1f6a69e35a2d284
fengweibin <tx_xinxin2006@163.com>
2018-01-17 15:31:54 +0800

    增加系统配置模块

---
+++ src/views/monitor/configure/model.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2017/9/22.
+ */
+const FormModel = {
+  mail: {name: '', value: 'mail value', id: '', type: '', desc: ''},
+  address: {name: '', value: 'address value', id: '', type: '', desc: ''}
+}
+export default FormModel
