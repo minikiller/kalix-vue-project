c71c9bc27ef2e55a257f4635454303e79d3c20af
fengweibin <tx_xinxin2006@163.com>
2018-01-23 09:15:38 +0800

    添加baseHelp组件

---
+++ src/components/custom/baseHelp.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+  el-popover(v-bind:ref="popover" v-bind:placement="top-start" v-bind:title="title" v-bind:width="width" v-bind:trigger="trigger")
+    i(style="margin-right: 10px;" v-bind:class="classname" slot="reference")
+</template>
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      popover: {
+        type: String, default: ''
+      },
+      placement: {
+        type: String, default: 'top-start'
+      },
+      title: {
+        type: String, default: ''
+      },
+      width: {
+        type: String, default: '200'
+      },
+      trigger: {
+        type: String, default: 'hover'
+      },
+      classname: {
+        type: String, default: 'el-icon-question'
+      }
+    },
+    data() {
+      return {
+      }
+    },
+    mounted() {
+    },
+    methods: {
+    },
+    watch: {
+    }
+  }
+</script>
+<style scoped lang="stylus" type="text/stylus">
+</style>
