70e45913add04097b4f9593e090abe2fe80d590f
houqingjie <150091225@qq.com>
2018-01-26 15:24:06 +0800

    修改红头文件文号类型字段使用及审批选项显示

--- src/views/oa/redheadapply/model.js
+++ src/views/oa/redheadapply/model.js
@@ -10,7 +10,7 @@ const FormModel = {
   applyDate: '',              // 工作流业务申请时间
   creationDate: '',           // 申请日期
   createBy: '',               // 申请人
-  docAssort: [],          // 文件种类
+  // docAssort: [],          // 文件种类
   docType: '',       // 发文类型
   editDocType: true, // 是否允许修改发文类型字段，默认t允许修改
   docContent: '',    // 发文内容
