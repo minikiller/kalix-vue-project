e06bf14acb53c5ab9fad544280bce9231c0efc25
houqingjie <150091225@qq.com>
2018-01-29 09:58:04 +0800

    修改红头文件流程过程中编辑时，编辑内容调整（包括显示样式和编辑控制）

--- src/views/oa/redheadapply/redheadapply.vue
+++ src/views/oa/redheadapply/redheadapply.vue
@@ -80,25 +80,7 @@
           {id: 'add', dialog: 'OaRedheadApplyAdd'},
           {id: 'progress', dialog: 'OaTaskView'},
           {id: 'preview', dialog: 'OaRedheadPreview'}
-        ],
-        formModel: {
-          title: '吉林动画学院红头文件申请表',
-          orgId: '',
-          orgName: '',
-          creationDate: '',
-          usageCount: '',
-          sealType: '',
-          sealTypeName: '',
-          createBy: '',
-          auditResult: '',
-          currentNode: '',
-          departmentHead: '', // 部门负责人
-          tableFormField: '', // 分公司负责人
-          counsel: '', // 法律顾问
-          generalManager: '', // 总经理
-          sealAdministrator: '', // 发文专管员
-          remark: '' // 发文专管员
-        }
+        ]
       }
     },
     mounted() {
