f5be4fed3434615c20033409fcdba54bb3b7c799
fengweibin <tx_xinxin2006@163.com>
2018-01-29 15:53:51 +0800

    增加配置管理模块

---
+++ src/views/admin/config/model.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Administrator on 2017/9/22.
+ */
+const FormModel = {
+  DB_NAME: {name: '', value: 'mail value', id: '', type: '', desc: ''},
+  Exist: {name: '', value: 'address value', id: '', type: '', desc: ''},
+  PROTOCOL: {name: '', value: 'address value', id: '', type: '', desc: ''},
+  IP: {name: '', value: 'address value', id: '', type: '', desc: ''},
+  PORT: {name: '', value: 'address value', id: '', type: '', desc: ''},
+  USER: {name: '', value: 'address value', id: '', type: '', desc: ''},
+  PASSWORD: {name: '', value: 'address value', id: '', type: '', desc: ''},
+  COUCHDB_URL: {name: '', value: 'address value', id: '', type: '', desc: ''}
+}
+export default FormModel
