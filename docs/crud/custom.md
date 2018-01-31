db7619789e5748852bda09812aac661376790fd5
yangze8818 <yangze_lmkj@163.com>
2018-01-31 08:08:43 +0800

    完成系统管理-功能管理用例开发

--- docs/crud/custom.md
+++ docs/crud/custom.md
@@ -1,5 +1,6 @@
 !> 开发过程中，经常会对table中的数据进行自定义的显示，下面以`views/common/receiver/receiver.vue`为例进行说明。
 ## 模板说明
+### 表结构
 ```js
 <template lang="pug">
   keep-alive
@@ -15,9 +16,19 @@
     bizSearch="CommonReceiverSearch")
 </template>
 ```
+### 树表结构
+```js
+<template lang="pug">
+    kalix-tree-grid(title="功能管理"
+    v-bind:columns='columns'
+    v-bind:targetURL="treeUrl"
+    v-bind:customRender="customRender")
+</template>
+```
 其中 `v-bind:customRender="customRender"` 即为自定义数据列的声明。
 
 ## 代码说明
+### 表结构实现
 ```js
   export default {
     methods: {
@@ -27,5 +38,21 @@
             })
           }
         }
+```
+### 树表结构实现
+```js
+  export default {
+    methods: {
+      customRender(_data) {
+        if (_data) {
+          _data.forEach((e) => {
+            e.isRead = e.read ? '是' : '否'
+            if (e.children) {
+              this.customRender(e.children)
+            }
+          })
+        }
+      }
+    }
 ```
 receiver业务数据中，有一个字段是read，类型为boolean型，用于显示该信息是否阅读过。通过循环新增isRead字段，就把read的内容翻译成了是否。
---
+++ src/views/admin/function/functionAdd.vue
@@ -0,0 +1,77 @@
+<!--
+描述：系统应用-功能管理-添加
+开发人：yangz
+开发日期：2018年01月25日
+-->
+<template lang="pug">
+  kalix-dialog.user-add(bizKey="function" ref="kalixBizDialog" v-bind:submitBefore="submitBefore"
+    v-bind:formModel.sync="formModel" v-bind:targetURL="targetURL")
+    div.el-form(slot="dialogFormSlot")
+      input(v-model="formModel.parentId" type="hidden")
+      input(v-model="formModel.isLeaf" type="hidden")
+      input(v-model="formModel.permission" type="hidden")
+      input(v-model="formModel.applicationId" type="hidden")
+      el-form-item(label="所属应用" prop="applicationName" label-width="120px" v-bind:rules="rules.applicationName")
+        el-input(v-model="formModel.applicationName" readonly)
+      el-form-item(label="上级功能" prop="parentName" label-width="120px" v-bind:rules="rules.parentName")
+        el-input(v-model="formModel.parentName" readonly)
+      el-form-item(label="名称" prop="name" label-width="120px" v-bind:rules="rules.name")
+        el-input(v-model="formModel.name")
+      el-form-item(label="功能代码" prop="code" label-width="120px" v-bind:rules="rules.code")
+        el-input(v-model="formModel.code")
+      el-form-item(label="数据权限主键" prop="dataPermissionKey" label-width="120px")
+        el-input(v-model="formModel.dataPermissionKey")
+      el-form-item(label="权限是否生效" prop="dataPermission" label-width="120px")
+        el-select(v-model="formModel.dataPermission" placeholder="请选择")
+          el-option(v-for="item in options" v-bind:key="item.value" v-bind:value="item.value" v-bind:label="item.label")
+      el-form-item(label="备注" prop="remark" label-width="120px")
+        el-input(v-model="formModel.remark" type="textarea" v-bind:rows="3")
+</template>
+
+<script type="text/ecmascript-6">
+  import FormModel from './model'
+  import Dialog from '@/components/custom/baseDialog.vue'
+  import {functionURL} from '../config.toml'
+  export default {
+    data() {
+      return {
+        formModel: Object.assign({}, FormModel),
+        rules: {
+          applicationName: [{required: true, message: '所属应用不能为空', trigger: 'blur'}],
+          parentName: [{required: true, message: '上级功能不能为空', trigger: 'blur'}],
+          name: [{required: true, message: '请输入名称', trigger: 'blur'}],
+          code: [{required: true, message: '请输入功能代码', trigger: 'blur'}]
+        },
+        targetURL: functionURL,
+        labelWidth: '110px',
+        options: [{
+          value: 'true',
+          label: '是'
+        }, {
+          value: 'false',
+          label: '否'
+        }]
+      }
+    },
+    components: {
+      KalixDialog: Dialog
+    },
+    computed: {
+    },
+    created() {
+    },
+    methods: {
+      init(dialogOption) {
+      },
+      submitBefore(baseDialog, callBack) {
+        let code = baseDialog.formModel.code
+        baseDialog.formModel.permission = baseDialog.formModel.permission + code
+        callBack()
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
