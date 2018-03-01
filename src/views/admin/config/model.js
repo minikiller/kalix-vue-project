6a98694811d0e94ddb6c4b51a5c0ecd245ad8db1
fengweibin <tx_xinxin2006@163.com>
2018-03-01 10:13:31 +0800

    修改数据格式

--- src/views/admin/config/model.js
+++ src/views/admin/config/model.js
@@ -2,13 +2,13 @@
  * Created by Administrator on 2017/9/22.
  */
 const FormModel = {
-  DB_NAME: {name: '', value: 'mail value', id: '', type: '', desc: ''},
-  Exist: {name: '', value: 'address value', id: '', type: '', desc: ''},
-  PROTOCOL: {name: '', value: 'address value', id: '', type: '', desc: ''},
-  IP: {name: '', value: 'address value', id: '', type: '', desc: ''},
-  PORT: {name: '', value: 'address value', id: '', type: '', desc: ''},
-  USER: {name: '', value: 'address value', id: '', type: '', desc: ''},
-  PASSWORD: {name: '', value: 'address value', id: '', type: '', desc: ''},
-  COUCHDB_URL: {name: '', value: 'address value', id: '', type: '', desc: ''}
+  DB_NAME: {name: '', value: 'mail value', id: '', type: '', desc: '', classType: ''},
+  Exist: {name: '', value: 'address value', id: '', type: '', desc: '', classType: ''},
+  PROTOCOL: {name: '', value: 'address value', id: '', type: '', desc: '', classType: ''},
+  IP: {name: '', value: 'address value', id: '', type: '', desc: '', classType: ''},
+  PORT: {name: '', value: 'address value', id: '', type: '', desc: '', classType: ''},
+  USER: {name: '', value: 'address value', id: '', type: '', desc: '', classType: ''},
+  PASSWORD: {name: '', value: 'address value', id: '', type: '', desc: '', classType: ''},
+  COUCHDB_URL: {name: '', value: 'address value', id: '', type: '', desc: '', classType: ''}
 }
 export default FormModel
