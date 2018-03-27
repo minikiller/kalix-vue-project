e0e890c8334dec1e3a32b4e4e5638e5ce42cfe4c
houqingjie <150091225@qq.com>
2018-03-27 15:46:56 +0800

    优化手机号码校验规则，同时新增固定电话校验组件并在用户新增和编辑模块中使用

--- src/views/admin/user/userAdd.vue
+++ src/views/admin/user/userAdd.vue
@@ -16,8 +16,7 @@
             el-radio(label="女")
         el-form-item(label="邮箱" prop="email" v-bind:label-width="labelWidth")
           el-input(v-model="formModel.email")
-        el-form-item(label="固定电话" prop="phone" v-bind:label-width="labelWidth")
-          el-input(v-model="formModel.phone")
+        kalix-phone-form-item(v-model="formModel.phone" v-bind:label-width="labelWidth")
         kalix-mobile-form-item(v-model="formModel.mobile" v-bind:label-width="labelWidth")
       template(v-else-if="formModel.userType === 1")
         el-form-item(label="学号" prop="code" v-bind:rules="rules.code_xh" v-bind:label-width="labelWidth")
@@ -30,8 +29,7 @@
             el-radio(label="女")
         el-form-item(label="邮箱" prop="email" v-bind:rules="rules.email" v-bind:label-width="labelWidth")
           el-input(v-model="formModel.email")
-        el-form-item(label="固定电话" prop="phone" v-bind:label-width="labelWidth")
-          el-input(v-model="formModel.phone")
+        kalix-phone-form-item(v-model="formModel.phone" v-bind:label-width="labelWidth")
         kalix-mobile-form-item(v-model="formModel.mobile" required v-bind:label-width="labelWidth")
       template(v-else-if="formModel.userType === 3")
         el-form-item(label="企业组织机构代码" prop="code" v-bind:rules="rules.code_qy" v-bind:label-width="labelWidth")
@@ -40,8 +38,7 @@
           el-input(v-model="formModel.name")
         el-form-item(label="邮箱" prop="email" v-bind:rules="rules.email" v-bind:label-width="labelWidth")
           el-input(v-model="formModel.email")
-        el-form-item(label="固定电话" prop="phone" v-bind:rules="rules.phone" v-bind:label-width="labelWidth")
-          el-input(v-model="formModel.phone")
+        kalix-phone-form-item(v-model="formModel.phone" required v-bind:label-width="labelWidth")
         kalix-mobile-form-item(v-model="formModel.mobile" v-bind:label-width="labelWidth")
       template(v-else)
         // 暂无
@@ -62,6 +59,7 @@
   import Dialog from '@/components/custom/baseDialog.vue'
   import BaseDictSelect from '@/components/custom/baseDictSelect'
   import BaseUpload from '@/components/custom/baseUpload'
+  import BasePhoneFormItem from '@/components/custom/basePhoneFormItem'
   import BaseMobileFormItem from '@/components/custom/baseMobileFormItem'
 
   export default {
@@ -102,8 +100,7 @@
           email: [
             {required: true, message: '请输入邮箱地址', trigger: 'blur'},
             {type: 'email', message: '请输入正确的邮箱地址', trigger: 'blur,change'}
-          ],
-          phone: [{required: true, message: '请输入固定电话', trigger: 'blur'}]
+          ]
         },
         targetURL: usersURL,
         labelWidth: '140px'
@@ -113,6 +110,7 @@
       KalixDialog: Dialog,
       KalixDictSelect: BaseDictSelect,
       KalixUpload: BaseUpload,
+      KalixPhoneFormItem: BasePhoneFormItem,
       KalixMobileFormItem: BaseMobileFormItem
     },
     created() {
