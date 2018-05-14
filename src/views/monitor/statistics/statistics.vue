618bc3f9793779445299c7bc134ac074bcfb8fcc
fengweibin <tx_xinxin2006@163.com>
2018-05-14 09:07:35 +0800

    硬件统计

---
+++ src/views/monitor/statistics/statistics.vue
@@ -0,0 +1,46 @@
+<!--
+描述：硬件检测-统计信息
+开发人：fwb
+开发日期：2018年01月30日
+-->
+<template lang="pug">
+  div(id="charts")
+    keep-alive
+      base-chart(bizKey="statisticView" title="统计图表" v-bind:autoload="false"
+      v-bind:chartTargetUrl="targetUrl" width="100%" height="500px"
+      bizSearch="OaStatisticSearch" )
+</template>
+
+<script type="text/ecmascript-6">
+  import BaseChart from '@/components/custom/baseChart'
+ // import Cache from 'common/cache'
+  import {HardwareStatisticsURL, StatisticsComponent} from '../config.toml'
+  import {registerComponent} from '@/api/register'
+  registerComponent(StatisticsComponent)
+  export default {
+    data() {
+      return {
+        prop: undefined,
+        path: undefined,
+        option: undefined,
+        workflowStatus: undefined,
+        targetUrl: HardwareStatisticsURL
+      }
+    },
+    computed: {
+    },
+    components: {
+      BaseChart
+    },
+    created() {
+    },
+    mounted() {
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style scoped lang="stylus">
+
+</style>
---
+++ src/views/monitor/statistics/statisticsSearch.vue
@@ -0,0 +1,58 @@
+<template lang="pug">
+  keep-alive
+    Kalix-Search(:searchFields="searchFields" title="统计查询"   ref="statisticSearchRef")
+</template>
+
+<script type="text/ecmascript-6">
+  import BaseSearch from '@/components/custom/baseSearch.vue'
+  export default {
+    components: {
+      KalixSearch: BaseSearch
+    },
+    data() {
+      return {
+        searchFields: [
+          {label: '统计时间', prop: 'dateBegin', type: 'date', field: 'creationDate:begin:gt'},
+          {label: '-', prop: 'dateEnd', type: 'date', field: 'creationDate:end:lt'},
+          {label: '统计分类',
+            prop: 'chartType',
+            type: 'select',
+            field: 'chartSelectValue',
+            options: [
+              {value: '0', label: '异常信息统计'},
+              {value: '1', label: '异常趋势统计'},
+              {value: '2', label: '硬件信息统计'}
+            ],
+            defaultVal: '0'
+          },
+          {label: '硬件分类',
+            prop: 'hardwareType',
+            type: 'select',
+            field: 'hardwareSelectValue',
+            options: [
+              {value: '0', label: '硬盘'},
+              {value: '1', label: '操作系统'},
+              {value: '2', label: '内存'}
+            ],
+            defaultVal: '0'
+          }
+        ]
+      }
+    },
+    mounted() {
+    },
+    methods: {
+    },
+    watch: {
+      options:
+        function(val) {
+          console.log('111111111111111111')
+        }
+    }
+  }
+</script>
+
+<style scoped lang="stylus" type="text/stylus">
+
+</style>
+
